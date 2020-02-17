1be5e4d14a5b7ee6144bd9d4c37695b099201dd3
Manuel F. Dolz <dolzm@uji.es>
2020-02-17 16:12:55 +0100

    fixed pool

---
+++ models/simplemlp.py
@@ -0,0 +1,12 @@
+from NN_model import *
+from NN_layer import *
+
+def create_simplemlp(comm, tracing):
+    model = Model(comm, tracing)
+    model.add( Input(shape=(28, 28, 1)) )
+    model.add( Flatten() )#
+    model.add( FC(shape=(512,), activation="relu") )
+    model.add( FC(shape=(512,), activation="relu") )
+    model.add( FC(shape=(512,), activation="relu") )
+    model.add( FC(shape=(10,), activation="sigmoid") )
+    return model
