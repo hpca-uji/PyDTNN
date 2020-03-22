773def3b718718a31b3b3d0bb977ce816dbb0c96
Manuel F. Dolz <dolzm@uji.es>
2020-03-22 20:48:22 +0100

    New PyDTNN version

--- tests/run.sh
+++ tests/run.sh
@@ -14,9 +14,18 @@ LASTH=`echo $NUMPR - 1 | bc`
 HOSTS=$(for i in `seq 0 $LASTH`; do printf "%s%02d," ${NODETYPE} ${i}; done)
 
 mpirun -genv LD_PRELOAD $EXTRAELIB \
-       -hosts $HOSTS -ppn 1 -np $NUMPR python -u benchmarks_CNN.py \
-       --batch_size 64 --steps_per_epoch 5 --num_epochs 1 \
-       --model vgg11 --dataset imagenet --parallel data
-       #--batch_size 64 --num_epochs 200 \
-      # --model simplecnn --dataset mnist --parallel data
-      # --batch_size 64 --num_epochs 1 \
+       -hosts $HOSTS -ppn 1 -np $NUMPR \
+       python3 -u benchmarks_CNN.py \
+          --model=alexnet \
+          --dataset=imagenet \
+          --dataset_train_path=/scratch/imagenet/np/train \
+          --dataset_test_path=/scratch/imagenet/np/validation \
+          --batch_size=64 \
+          --validation_split=0.2 \
+          --num_epochs=10 \
+          --optimizer=SGDMomentum \
+          --learning_rate=0.01 \
+          --loss_func=accuracy,categorical_cross_entropy \
+          --parallel=data \
+          --blocking_mpi \
+          --dtype=float32
