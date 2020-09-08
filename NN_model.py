cbac1e36fdddbc0e80832f772df33b84d24ee4b5
Manuel F. Dolz <dolzm@uji.es>
2020-09-08 13:21:02 +0200

    Added NCCL support

--- NN_model.py
+++ NN_model.py
@@ -103,12 +103,54 @@ class Model:
             sys.exit(-1)
 
         if self.enable_cudnn and supported_cudnn:
-            # import pycuda.autoinit
-            device_id = rank % drv.Device.count()
-            drv.init()
-            context = drv.Device(device_id).make_context()
-            import atexit
-            atexit.register(context.pop)
+            import pycuda.autoinit
+            # # Use this code if autoinit is not available
+            # device_id = self.rank % drv.Device.count()
+            # drv.init()
+            # context = drv.Device(device_id).make_context()
+            # import atexit
+            # atexit.register(context.pop)
+
+            if self.enable_nccl and self.comm and self.enable_cudnn:
+                types = {np.float64: nccl.DataType.Float64,
+                         np.float32: nccl.DataType.Float32,
+                         np.int8:    nccl.DataType.Int8,
+                         np.int32:   nccl.DataType.Int32}   
+        
+                try:    self.nccl_type = types[self.type]
+                except: self.nccl_type = nccl.DataType.Float32
+    
+                hostname = MPI.Get_processor_name()
+    
+                hosts_data = comm.allgather([self.rank, hostname])
+                # Build a dictionary hostname : [ranks_in_host]
+                #   { "host1" : [0, 1], "host2" : [2, 3] }
+                hosts = {}
+                for r, h in hosts_data:
+                   if not h in hosts: hosts[h] = [r]
+                   else: hosts[h].append(r)
+                
+                # Check that no more processes than GPUs per node are used
+                for host, ranks_in_host in hosts.items():
+                   assert len(ranks_in_host) <= self.params.gpus_per_node
+                
+                self.intra_ranks = hosts[hostname]
+                # Only a master process per node is selected as inter rank
+                self.inter_ranks = [r[0] for h, r in hosts.items()]
+                    
+                intra_group_ = comm.Get_group()
+                intra_group = MPI.Group.Incl(intra_group_, self.intra_ranks)
+                intra_comm = comm.Create(intra_group)
+                
+                if len(self.inter_ranks) > 1:
+                   inter_group_ = comm.Get_group()
+                   inter_group = MPI.Group.Incl(inter_group_, self.inter_ranks)
+                   self.inter_comm = comm.Create(inter_group)
+                
+                # Get an id once per master process and distribute it to all intra ranks
+                id = intra_comm.bcast(nccl.ncclGetUniqueId() if self.rank in self.inter_ranks else None)
+                self.nccl_comm = nccl.ncclCommInitRank(len(self.intra_ranks), id, intra_comm.Get_rank())            
+
 
             self.cudnn_handle = cudnn.cudnnCreate()
             self.cublas_handle = cublas.cublasCreate()
@@ -133,46 +175,6 @@ class Model:
             print("or you must install pycuda+skcuda to allow GPU GEMMs executions!")
             sys.exit(-1)
 
-        if self.enable_nccl and self.comm and self.enable_cudnn:
-
-            types = {np.float64: nccl.DataType.Float64,
-                     np.float32: nccl.DataType.Float32,
-                     np.int8:    nccl.DataType.Int8,
-                     np.int32:   nccl.DataType.Int32}   
-    
-            try:    self.nccl_type = types[self.type]
-            except: self.nccl_type = nccl.DataType.Float32
-
-            hostname = MPI.Get_processor_name()
-
-            hosts_data = comm.allgather([self.rank, hostname])
-            # Build a dictionary hostname : [ranks_in_host]
-            #   { "host1" : [0, 1], "host2" : [2, 3] }
-            hosts = {}
-            for r, h in hosts_data:
-               if not h in hosts: hosts[h] = [r]
-               else: hosts[h].append(r)
-            
-            # Check that no more processes than GPUs per node are used
-            for host, ranks_in_host in hosts.items():
-               assert len(ranks_in_host) <= self.params.gpus_per_node
-            
-            self.intra_ranks = hosts[hostname]
-            # Only a master process per node is selected as inter rank
-            self.inter_ranks = [r[0] for h, r in hosts.items()]
-                
-            intra_group_ = comm.Get_group()
-            intra_group = MPI.Group.Incl(intra_group_, self.intra_ranks)
-            intra_comm = comm.Create(intra_group)
-            
-            if len(self.inter_ranks) > 1:
-               inter_group_ = comm.Get_group()
-               inter_group = MPI.Group.Incl(inter_group_, self.inter_ranks)
-               self.inter_comm = comm.Create(inter_group)
-            
-            # Get an id once per master process and distribute it to all intra ranks
-            id = intra_comm.bcast(nccl.ncclGetUniqueId() if self.rank in self.inter_ranks else None)
-            self.nccl_comm = nccl.ncclCommInitRank(len(self.intra_ranks), id, intra_comm.Get_rank())            
 
         elif self.enable_nccl:
             self.enable_nccl = False
