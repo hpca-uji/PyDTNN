d560e910249e089175c0fe08f64033d5660431a1
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2021-01-05 12:00:28 +0100

    Parametrized versions of run_alexnet_cifar10 tests for convGemm evaluation

--- tests/run_alexnet_cifar10_no_conv_gemm.sh
+++ tests/run_alexnet_cifar10_no_conv_gemm.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 
-export OMP_NUM_THREADS=4
-python3 -u tests/benchmarks_CNN.py \
-         --model=alexnet_cifar10 \
-         --dataset=cifar10 \
-         --dataset_train_path=/home/barrachi/Descargas/data/cifar-10-batches-bin/ \
-         --dataset_test_path=/home/barrachi/Descargas/data/cifar-10-batches-bin/ \
-         --test_as_validation=False \
-         --batch_size=64 \
-         --validation_split=0.2 \
-         --steps_per_epoch=0 \
-         --num_epochs=30 \
-         --evaluate=True \
-         --optimizer=sgd \
-         --learning_rate=0.01 \
-         --momentum=0.9 \
-         --loss=categorical_cross_entropy \
-         --metrics=categorical_accuracy \
-         --lr_schedulers=early_stopping,reduce_lr_on_plateau \
-         --warm_up_epochs=5 \
-         --early_stopping_metric=val_categorical_cross_entropy \
-         --early_stopping_patience=10 \
-         --reduce_lr_on_plateau_metric=val_categorical_cross_entropy \
-         --reduce_lr_on_plateau_factor=0.1 \
-         --reduce_lr_on_plateau_patience=5 \
-         --reduce_lr_on_plateau_min_lr=0 \
-         --parallel=sequential \
-         --non_blocking_mpi=False \
-         --tracing=False \
-         --profile=False \
-         --enable_gpu=False \
-         --dtype=float32 \
-         --enable_conv_gemm=False
+export ENABLE_CONV_GEMM=False
+
+SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 || exit 1; pwd -P)"
+exec "${SCRIPT_PATH}"/run_alexnet_cifar10_conv_gemm.sh
