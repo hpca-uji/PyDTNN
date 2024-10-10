6b2f3620a692327da9b877f62009516171d0336a
mipardo <mipardo@uji.es>
2024-10-10 22:54:59 +0200

    fix: removed weights allreduce when ok-topk optimizer is being used

--- pydtnn/model.py
+++ pydtnn/model.py
@@ -559,7 +559,8 @@ class Model:
 
             # Weight update (WU)
             for i in range(len(self.layers) - 1, 0, -1):
-                self.layers[i].reduce_weights_sync()
+                if self.optimizer.__class__.__name__ != 'SGD_OkTopkCPU':
+                    self.layers[i].reduce_weights_sync()
                 self.tracer.emit_event(PYDTNN_MDL_EVENT, self.layers[i].id * PYDTNN_MDL_EVENTS + PYDTNN_MDL_UPDATE_DW)
                 self.layers[i].update_weights(self.optimizer)
                 self.tracer.emit_event(PYDTNN_MDL_EVENT, 0)
