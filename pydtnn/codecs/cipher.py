ccb8fce4aafc70afed847adb1a934c838e916dde
Miguel Ángel Prosper <mprosper@uji.es>
2025-02-26 11:41:31 +0100

    codecs: add codecs submodule with various data transformes

---
+++ pydtnn/codecs/cipher.py
@@ -0,0 +1,38 @@
+"""Ciphertext codec"""
+
+from dataclasses import dataclass
+
+
+__all__ = (
+    "Codec",
+)
+
+
+@dataclass(eq=False, order=False, slots=True, frozen=True)
+class Ciphertext[T]:
+    """Pseudo-ciphertext"""
+    plain: T
+
+    def __add__(self, other):
+        """Add two ciphertexts"""
+        if not isinstance(other, Ciphertext):
+            raise NotImplementedError()
+        return Ciphertext[T](self.plain + other.plain)
+
+    def __mul__(self, other):
+        """Multiply two ciphertexts"""
+        if not isinstance(other, Ciphertext):
+            raise NotImplementedError()
+        return Ciphertext[T](self.plain * other.plain)
+
+
+class Codec:
+    """Ciphertext codec"""
+
+    def encode(self, obj) -> Ciphertext:
+        """Encode object to ciphertext"""
+        return Ciphertext(obj)
+
+    def decode(self, obj: Ciphertext):
+        """Decode cypertext to object"""
+        return obj.plain
