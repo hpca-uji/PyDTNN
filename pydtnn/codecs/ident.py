5e7e92a09940bb67fe75d8a08b63e83d917a249d
Miguel Ángel Prosper <mprosper@uji.es>
2025-02-26 11:46:58 +0100

    codecs: add identity codec

---
+++ pydtnn/codecs/ident.py
@@ -0,0 +1,18 @@
+"""Identidy codec"""
+
+
+__all__ = (
+    "Codec",
+)
+
+
+class Codec:
+    """Identity codec"""
+
+    def encode[T](self, obj: T) -> T:
+        """Encode object as itself"""
+        return obj
+
+    def decode[T](self, obj: T) -> T:
+        """Decode object as itself"""
+        return obj
