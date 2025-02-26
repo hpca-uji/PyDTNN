86c87d75f71c79c3b273619d0acd097558fea28c
Miguel Ángel Prosper <mprosper@uji.es>
2025-02-26 12:02:09 +0100

    codecs: rename cipher to fhe and provide better typing

---
+++ pydtnn/codecs/__init__.py
@@ -0,0 +1,11 @@
+"""Codecs module"""
+
+from pydtnn.codecs import array, fhe, ident, python
+
+
+__all__ = (
+    "array",
+    "fhe",
+    "ident",
+    "python"
+)
---
+++ pydtnn/codecs/fhe.py
@@ -0,0 +1,41 @@
+"""Fully Homomorphic Encryption codec"""
+
+# NOTE: Module does not provide any encryption, it is temporary placeholder
+
+import numpy
+from dataclasses import dataclass
+
+
+__all__ = (
+    "Codec",
+)
+
+
+@dataclass(repr=False, eq=False, order=False, slots=True, frozen=True)
+class Ciphertext:
+    """Pseudo-ciphertext"""
+    _plain: numpy.ndarray
+
+    def __add__(self, other):
+        """Add two ciphertexts"""
+        if not isinstance(other, Ciphertext):
+            raise NotImplementedError()
+        return Ciphertext(self._plain + other._plain)
+
+    def __mul__(self, other):
+        """Multiply two ciphertexts"""
+        if not isinstance(other, Ciphertext):
+            raise NotImplementedError()
+        return Ciphertext(self._plain * other._plain)
+
+
+class Codec:
+    """Fully Homomorphic Encryption codec"""
+
+    def encode(self, obj: numpy.ndarray) -> Ciphertext:
+        """Encode object to ciphertext"""
+        return Ciphertext(obj)
+
+    def decode(self, obj: Ciphertext) -> numpy.ndarray:
+        """Decode cypertext to object"""
+        return obj._plain
