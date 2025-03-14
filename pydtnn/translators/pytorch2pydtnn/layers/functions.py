- Fixed the "Adaptative Average Pooling 2D" pool shape and stride calculations. - Added some debbuging prints (TODO: when it works well, remove them). - It fails, with some models during the training, with other models, during "rebuilding" of the model as a PyDTNN one

pydtnn/translators/pytorch2pydtnn/layers/functions.py
@@ -19,7 +19,7 @@ from pydtnn.translators.pytorch2pydtnn.layers.pooling import *
 # TODO: Check how to do this well.
 def Add(args: Dict[str, Any]) -> Tuple[layers.AdditionBlock, str]:
     # https://pytorch.org/docs/stable/generated/torch.add.html      
-    print(f"Layer: {stack()[0].function}")
+    print(f"Function: {stack()[0].function}")
     
     # It should be prepared so the params have the following format: "[layer1,layer2]"
     layer_name: str = args[cons.OPERATION_VAR]
@@ -29,7 +29,7 @@ def Add(args: Dict[str, Any]) -> Tuple[layers.AdditionBlock, str]:
     params = cons.get_equivalent_layer(params, dict_equivalent_layers)
     dict_layers: Dict[str, Tuple[LayerAndActivationBase, str]] = args[cons.LAYERS]
 
-    # PROBLEMA: List_layers sale al revés
+
     list_layers, to_remove, input_layer_name = cons.get_lists_operations_and_outputs(dict_layers=dict_layers, layer_inputs=params) 
 
     to_remove = set(to_remove) # Remove multiple ocurrences of a layer. Consecuence of "get_equivalent_layer".
@@ -50,7 +50,7 @@ def Add(args: Dict[str, Any]) -> Tuple[layers.AdditionBlock, str]:
 def Concat(args: Dict[str, Any]) -> Tuple[layers.ConcatenationBlock, str]:
     # https://pytorch.org/docs/main/generated/torch.cat.html
     
-    print(f"Layer: {stack()[0].function}")
+    print(f"Function: {stack()[0].function}")
 
     # TODO: es necesario hacer un diccionario que sustituya los parámetros que ya han sido introducidos por la capa de concatenación/adición.
     # También hay que haer que solo aparezca una única vez.
@@ -88,6 +88,7 @@ def Flatten(args: Dict[str, str]) -> Tuple[layers.Flatten, str]:
     # https://pytorch.org/docs/stable/generated/torch.flatten.html
     #from torch import flatten
     # torch.flatten(input, start_dim=0, end_dim=-1)
+    print(f"Function: {stack()[0].function}")
 
     def switch(list_params: List[str], dict_params: Dict[str, str] = dict()) -> Dict[str, str]:
         match len(list_params):
@@ -109,7 +110,7 @@ def Flatten(args: Dict[str, str]) -> Tuple[layers.Flatten, str]:
                 return dict_params
     # - END switch - #
 
-    print(f"Layer: {stack()[0].function}")
+    print(f"Function: {stack()[0].function}")
     params = args[cons.PARAMETERS].strip()
     dict_params = switch(params.split(cons.ARGS_SEPARATOR))
         
@@ -122,7 +123,7 @@ def relu(args: Dict[str, str]) -> Tuple[activations.Relu, str]:
     # from torch.nn.functional import relu
     # relu(input: Tensor, inplace: bool = False)
     
-    print(f"Layer: {stack()[0].function}")
+    print(f"Function: {stack()[0].function}")
 
     dict_params = dict()
 
@@ -130,18 +131,19 @@ def relu(args: Dict[str, str]) -> Tuple[activations.Relu, str]:
     params = args[cons.PARAMETERS].strip().split("inplace=")
     inplace = bool(params.pop()) if len(params) > 0 else None
 
-    dict_params["input"] = params[0].split(cons.ARGS_SEPARATOR)[0]
+    dict_params[cons.ARGUMENTS] = {"input": params[0].split(cons.ARGS_SEPARATOR)[0]}
     if inplace is not None:
         dict_params["inplace"] = inplace
 
-    return (ReLU(dict_params), dict_params["input"])
+    return (ReLU(dict_params), dict_params[cons.ARGUMENTS]["input"])
 
 def adaptive_avg_pool_2d(args: Dict[str, str]) -> Tuple[layers.AveragePool2D, str]:
     # It is not the layer, but the operation itself.
     # from torch.nn.functional import adaptive_avg_pool2d
     # adaptive_avg_pool2d(input: Tensor, output_size: BroadcastingList2[int])
 
-    print(f"Layer: {stack()[0].function}")
+    print(f"Function: {stack()[0].function}")
+
     dict_params = dict()
     # Example: torch.nn.functional.adaptive_avg_pool2d(relu, (1, 1)) | args = 'relu, (1, 1)'
     params:List[str] = args[cons.PARAMETERS].split(cons.ARGS_SEPARATOR)

pydtnn/translators/pytorch2pydtnn/layers/pooling.py
@@ -9,7 +9,8 @@ from inspect import stack # This is only in order to get the function's name
 # Functionality imports
 from pydtnn import layers
 import pydtnn.translators.pytorch2pydtnn.constats as cons
-
+import numpy as np
+from pydtnn.utils import decode_tensor, encode_tensor
 
 # ------------------- #
 # ---- CONSTANTS ---- #
@@ -51,6 +52,9 @@ def MaxPool2d(args: Dict[str, Any]) -> layers.MaxPool2D:
         # else: It must be a Tuple[int, int], so it's okay
     # else: Nothing special
 
+    cons.print_dict(args[cons.ARGUMENTS], "args[cons.ARGUMENTS]")
+    cons.print_dict(layer_args, "layer_args")
+
     return layers.MaxPool2D(**layer_args)
 # --- END MaxPool2d --- #
 
@@ -73,7 +77,10 @@ def AvgPool2d(args: Dict[str, Any]) -> layers.AveragePool2D:
         pool_shape = layer_args[PYDTNN_POOL_SHAPE]
         if isinstance(pool_shape, int):
             layer_args[PYDTNN_POOL_SHAPE] = (pool_shape, pool_shape)
-        #else: It mus be a Tuple[int, int], so it's okay
+        #else: It must be a Tuple[int, int], so it's okay
+
+    cons.print_dict(args[cons.ARGUMENTS], "args[cons.ARGUMENTS]")
+    cons.print_dict(layer_args, "layer_args")
 
     return layers.AveragePool2D(**layer_args)
 # --- END AvgPool2d --- #
@@ -82,18 +89,82 @@ def AdaptiveAvgPool2d(args: Dict[str, Any]) -> layers.AveragePool2D:
     # https://pytorch.org/docs/stable/generated/torch.nn.AdaptiveAvgPool2d.html#torch.nn.AdaptiveAvgPool2d
     # from torch.nn import AdaptiveAvgPool2d    
     # NOTE: "The output is of size H x W, for any input size. The number of output features is equal to the number of input planes." Source: PyTorch.      
-    
+    print(f"Layer: {stack()[0].function}")
+
     arguments = args[cons.ARGUMENTS]    
-    _output_size = arguments[cons.PYTORCH_OUTPUT_SIZE] if cons.PYTORCH_OUTPUT_SIZE in arguments else None
-    
+    output_size = arguments[cons.PYTORCH_OUTPUT_SIZE] if cons.PYTORCH_OUTPUT_SIZE in arguments else None
+
+    print("AAA")
+    cons.print_dict(arguments, "arguments")
+    print(f"output_size: {output_size}")    
+
     # TODO: Check if there is a way to avoid this (~terrorism~) weird patch.
     layer = layers.AveragePool2D()
 
     base_initialize = layer.initialize
     
     # NOTE: IMPORTANT if the base function's arguments change, it is necessary to change them here too.
+    def new_initialize(prev_shape, need_dx=True, *, output_size = output_size, layer = layer, base_initialize = base_initialize):
+        
+        print("AAA")
+        print(f"output_size: {output_size}")
+
+        layer.hi, layer.wi, layer.ci = decode_tensor(prev_shape, layer.model.tensor_format)
+        if output_size is not None:
+            layer.ho, layer.wo = (output_size, output_size) if isinstance(output_size, int) else output_size
+        else:
+            layer.ho = layer.hi
+            layer.wo = layer.wi
+        layer.co = layer.ci
+
+        layer.padding = 0
+        layer.dilation = 1
+
+        layer.vpadding, layer.hpadding = (layer.padding, layer.padding)
+        layer.vdilation, layer.hdilation = (layer.dilation, layer.dilation)
+
+        # Unknown values: pool_shape (kh, kw) and stride (vstride, hstride)
+
+        # Getting (and setting) the pool_shape (kh, kw)
+        layer.kh = layer.hi // layer.ho
+        layer.kw = layer.wi // layer.wo
+
+        print(f"layer.ci: {layer.ci}")
+        print(f"layer.hi: {layer.hi}")
+        print(f"layer.wi: {layer.wi}")
+        print(f"layer.co: {layer.co}")
+        print(f"layer.ho: {layer.ho}")
+        print(f"layer.wo: {layer.wo}")
+        print(f"layer.kh: {layer.kh}")
+        print(f"layer.kw: {layer.kw}")
+        print(f"layer.vpadding: {layer.vpadding}")
+        print(f"layer.hpadding: {layer.hpadding}")
+        print(f"layer.vdilation: {layer.vdilation}")
+        print(f"layer.hdilation: {layer.hdilation}")
+
+        # Getting (and setting) the stride (vstride, hstride)        
+        # Base formula: self.ho = (self.hi + 2 * self.vpadding - self.vdilation * (self.kh - 1) - 1) // self.vstride + 1
+        layer.vstride = (layer.hi + 2 * layer.vpadding - layer.vdilation * (layer.kh - 1) - 1) // (layer.ho - 1)
+        # Base formula: self.wo = (self.wi + 2 * self.hpadding - self.hdilation * (self.kw - 1) - 1) // self.hstride + 1
+        layer.hstride = (layer.wi + 2 * layer.hpadding - layer.hdilation * (layer.kw - 1) - 1) // (layer.wo - 1)
+        
+
+        print(f"layer.vstride: {layer.vstride}")
+        print(f"layer.hstride: {layer.hstride}")
+
+        #base_initialize(prev_shape, need_dx)
+    # -- END new_initialize -- #
+    
+    layer.initialize = new_initialize
+
+    return layer
+# --- END AdaptiveAvgPool2d --- #
+
+
+# NOTE: IMPORTANT if the base function's arguments change, it is necessary to change them here too.
     def new_initialize(prev_shape, need_dx=True, *, _output_size = _output_size, layer = layer, base_initialize = base_initialize):
         
+        print("AAAAA")
         # Information used in order to make this function's operations:
         # https://stackoverflow.com/questions/58692476/what-is-adaptive-average-pooling-and-how-does-it-work 
         # https://stackoverflow.com/questions/53841509/how-does-adaptive-pooling-in-pytorch-work/63603993#63603993
@@ -102,26 +173,60 @@ def AdaptiveAvgPool2d(args: Dict[str, Any]) -> layers.AveragePool2D:
         #   input_size: | type: <class 'tuple'> | value: (31, 31, 4)
         #   output_size: | type: <class 'list'> | value: [4, 4]
 
+        _output_size = layer.shape
+        print(f"_output_size: {_output_size}")
+
+        # TODO: Mirar cómo llegan las cosas en el Flatten de CPU para ver si esto tiene sentido
         input_size = prev_shape
         # It's possible that it cames with the format 4 ==> transform to [4,4] and operate.
         if _output_size is not None:
             output_size = (_output_size, _output_size) if isinstance(_output_size, int) else _output_size
         else:
-            # Seems that, internally the format is: "PYDTNN_TENSOR_FORMAT_NHWC" [TODO: Check this affirmation] ==> 
-            #   ==> The N dimension is implicit and the C dimension must ot be ignored
-            output_size = input_size[:-1]           
+            output_size = input_size
+        
+        layer.padding= 0 
+        layer.stride= 2 
+        layer.dilation= 1
+        layer.pool_shape = (2, 2)
 
-        #layer.stride = input_size // output_size
-        len_output_size = len(output_size)
-        layer.stride = tuple([ input_size[i] // output_size[i] for i in range(len_output_size)])
-
-        #layer.pool_shape = input_size - (output_size - 1) * layer.stride  
-        layer.pool_shape = [0] * len_output_size
-        for i in range(len_output_size):
-            layer.pool_shape[i] = input_size[i] - (output_size[i] - 1) * layer.stride[i]
-        layer.pool_shape = tuple(layer.pool_shape)
-
+        len_output_size = len(output_size)        
+        #layer.stride = tuple([ input_size[i] // output_size[i] for i in range(len_output_size)])
+        layer.pool_shape = tuple([ input_size[i] // output_size[i] for i in range(len_output_size)])
+        layer.stride = tuple(((input_size[i] - layer.pool_shape[i]) // (output_size[i] - 1)) if output_size[i] > 1 
+                             else layer.pool_shape[i]                               
+                             for i in range(len_output_size))
+        # layer.pool_shape = [0] * len_output_size        
+        # for i in range(len_output_size):
+        #     layer.pool_shape[i] = input_size[i] - (output_size[i] - 1) * layer.stride[i]
+        # layer.pool_shape = tuple(layer.pool_shape)
+        layer.pool_shape = tuple(input_size[i] - (output_size[i] - 1) * layer.stride[i] for i in range(len_output_size))        
         layer.padding = 0
+
+        # TODO: 
+        a = r"Ni idea de cómo hacer esto."
+        "IDEA: Pasar de la variable, ver el tamaño de la capa actual, ver el tamaño de la capa anterior y calcularlo todo así"
+        """
+        x.shape: (64, 512, 2, 2)
+        self.shape: (512, 1, 1)
+        self.prev_shape: (512, 2, 2)
+        self.pool_shape: (2, 2)
+        self.padding: 0
+        self.stride: 2
+        self.dilation: 1
+        self.co: 512
+        self.ho: 1
+        self.wo: 1
+        
+        """
+
+        print(f"layer: {layer.shape}")
+        print(f"input_size: {input_size}")
+        print(f"_output_size: {_output_size}")
+        print(f"output_size: {output_size}")
+        print(f"layer.stride: {layer.stride}")
+        print(f"layer.stride: {layer.stride}")
+        print(f"layer.pool_shape: {layer.pool_shape}")
+        print("---\n")
 
         # Calling directly "layer.initialize" will produce a recursive call (Remember: this function will be "layer.initialize")
         base_initialize(prev_shape = prev_shape, need_dx = need_dx)
@@ -129,5 +234,8 @@ def AdaptiveAvgPool2d(args: Dict[str, Any]) -> layers.AveragePool2D:
     
     layer.initialize = new_initialize
 
-    return layer
-# --- END AdaptiveAvgPool2d --- #
+
+
+
+
+

pydtnn/translators/pytorch2pydtnn/model_convertor.py
@@ -7,15 +7,32 @@ import numpy as np
 import torch
 from pydtnn.model import Model as PyDTNN_Model
 from pydtnn.layers import Input
-from pydtnn.utils import PYDTNN_TENSOR_FORMAT_NCHW
+from pydtnn.utils import PYDTNN_TENSOR_FORMAT_NCHW, PYDTNN_TENSOR_FORMAT_NHWC
 import pydtnn.translators.pytorch2pydtnn.constats as cons
+from pydtnn.utils import decode_tensor
+
+def modify_forward(layer: LayerAndActivationBase) -> None:
+    old_forward = layer.forward
+
+    def new_forward(x, layer=layer, old_forward=old_forward):
+        print(f"Layer: {layer}")
+        print(f"layer.shape: {layer.shape}")
+        print(f"layer.prev_shape: {layer.prev_shape}")
+        print(f"x.shape: {x.shape}")
+        print(f"layer.weights.shape: {layer.weights.shape}")
+        print(f"layer.biases.shape: {layer.biases.shape}")
+
+        old_forward(x)
+
+    layer.forward = new_forward
 
 def load_layers(model:PyDTNN_Model, layers: List[LayerAndActivationBase]) -> None:
+    # TODO: REMEMBER -> Put modify_forward to False (or remove it.)
 
     # TODO: Check if there are more operations to do.
     #   If not ==> Move to the main function.
     for layer in layers:
-        #print(layer)
+        print(layer)
         model.add(layer)
 # --- END load_layers --- #
 
@@ -63,10 +80,11 @@ def extract_layers_relations(model:torch.nn.Module) -> Dict[str, Tuple[Union[str
     TORCH_LAYER_REQ = "torch.nn.functional."
     TORCH_FUNC_REQ = "torch."
     # -- END CONSTANTS -- #
-
+    print("lines:")
     for line in filter(lambda x: not(FIRST_LINE in x or LAST_LINE in x) , 
                        filter(lambda x: len(x)!=0, 
                         [elem.lstrip(PSEUDO_INDENTATION) for elem in graph.code.split(BY_LINES)])):
+        print(line)
         # NOTE: seems that there are situations that the line does not have the value.
         line = line.split(SEPARATOR_FUNCTION_VALUE)[0] # [line, debug's input's value]            
         operation = line.split(SEPARATOR_ASSIGNATION)  # [output, function+args]
@@ -83,6 +101,7 @@ def extract_layers_relations(model:torch.nn.Module) -> Dict[str, Tuple[Union[str
 
         func = None # It will be assigned in the following if-else statement
         if len(operation) > 1:
+            print(f"operation: {operation}")
             # Normal case. Examples: 'getattr(self.layer1, "2").bn1(layer1_2_conv1)', 'self.avgpool(features_36)'       
             if any(MODEL_LAYER_REQ in part for part in operation):
                 # Case: 'getattr(self.layer1, "2").bn1(layer1_2_conv1)'
@@ -90,6 +109,9 @@ def extract_layers_relations(model:torch.nn.Module) -> Dict[str, Tuple[Union[str
                 operation = PARAMETERS_BEGINING.join(operation) # Reasembling the operation without the arguments.
                 operation = operation.replace(MODEL_LAYER_REQ, MODEL_FUNCT_ARG_NAME) 
                 func = eval(operation) # Getting the layer object.
+                print(f"args: {args}")
+                print(f"operation: {operation}")
+                print(f"func: {func}")
             else:
                 # Cases: function or layer not defined at model's object's constructor                
                 # TORCH_LAYER_REQ --> Case: layer not defined at model's object's constructor
@@ -182,7 +204,6 @@ def convert_layers_and_set_weights_and_biases(input_shape: Tuple[int], layers:Di
             if LAYER_WEIGHTS in state_dict:                        
                 # The weights are "torch.Tensor": torch.Tensor.cpu().detach().numpy() ==> weigths as np.array                
                 weights = copy.deepcopy(state_dict[LAYER_WEIGHTS].cpu().detach().numpy())
-                
                 if hasattr(converted_layer, PYDTNN_WEIGHTS_INITIALIZER):
                     # TODO: Check if there is another way to do this.
                     def weights_initializer(*args_to_ignore, pytorch_weights = weights):
@@ -233,14 +254,15 @@ def convert_model(model:torch.nn.Module, input_shape:Tuple[int], omm=None, non_b
     
     # NOTE: PyTorch's weight tensors use NCHW format.
     if "tensor_format" not in kwargs:
-        kwargs["tensor_format"] = PYDTNN_TENSOR_FORMAT_NCHW
+        kwargs["tensor_format"] = "NCHW"
     if "model_name" not in kwargs:
         kwargs["model_name"] = None
-
+    
     # Output model.
     converted_model = PyDTNN_Model(omm=omm, non_blocking_mpi=non_blocking_mpi, enable_gpu=enable_gpu, enable_gpudirect=enable_gpudirect,
-                    enable_nccl=enable_nccl, dtype=dtype, tracing=tracing, tracer_output=tracer_output, **kwargs)    
+                    enable_nccl=enable_nccl, dtype=dtype, tracing=tracing, tracer_output=tracer_output, **kwargs)
 
+    cons.MODELO = converted_model
     # Obtaining the model's layers/operations, activations, etc.; and the relation between them.
     dict_layers = extract_layers_relations(model = model)
 

pydtnn/translators/pytorch2pydtnn/test.py
@@ -1,9 +1,9 @@
 from model_convertor import convert_model
-import torch
+
 from torchvision.models import vgg19, alexnet, densenet169, resnet50, googlenet
 from torchvision.models import densenet121, densenet201, resnet18, resnet34, resnet101, resnet152, vgg11, vgg16
-from torchvision.models import VGG, AlexNet, DenseNet, ResNet, GoogLeNet
 
+from torchmetrics import Accuracy, AUROC, AveragePrecision, F1Score
 
 from pydtnn.activations import *
 from pydtnn.layers import *
@@ -11,7 +11,7 @@ from pydtnn.layers import *
 from pydtnn.models.vgg11 import create_vgg11
 from pydtnn.models.vgg16 import create_vgg16
 from pydtnn.models.vgg19_imagenet import create_vgg19_imagenet
-from pydtnn.models.alexnet import create_alexnet
+from pydtnn.models.alexnet_cifar10 import create_alexnet_cifar10
 from pydtnn.models.densenet121_cifar10 import create_densenet121_cifar10
 from pydtnn.models.densenet169_cifar10 import create_densenet169_cifar10
 from pydtnn.models.densenet201_cifar10 import create_densenet201_cifar10
@@ -23,55 +23,160 @@ from pydtnn.models.resnet152_cifar10 import create_resnet152_cifar10
 from pydtnn.models.inceptionv3_cifar10 import create_inceptionv3_cifar10
 
 from pydtnn.model import Model as PyDTNN_Model
+from pydtnn.datasets import get_dataset
+from pydtnn.utils import PYDTNN_TENSOR_FORMAT_NCHW, PYDTNN_TENSOR_FORMAT_NHWC
+from pydtnn.utils.best_of import BestOf
+
+import torch
+from torch.nn import CrossEntropyLoss
+
+DATASET_PATH = "/home/usuario/Documentos/CIBER_CAFE/Datasets/cifar-10-batches-bin"
 
 dict_test = {
-   "vgg11": (vgg11(), create_vgg11, (224, 224, 3)),
-   "vgg16": (vgg16(), create_vgg16, (224, 224, 3)),
-   "vgg19": (vgg19(), create_vgg19_imagenet, (224, 224, 3)), 
-   "alexnet": (alexnet(), create_alexnet, (227, 227, 3)), 
-   "densenet121": (densenet121(), create_densenet121_cifar10, (32, 32, 3)),
-   "densenet169": (densenet169(), create_densenet169_cifar10, (32, 32, 3)), 
-   "densenet201": (densenet201(), create_densenet201_cifar10, (32, 32, 3)),   
-   "resnet18": (resnet18(), create_resnet18_cifar10, (32, 32, 3)),
-   "resnet34": (resnet34(), create_resnet34_cifar10, (32, 32, 3)),
-   "resnet50": (resnet50(), create_resnet50_cifar10, (32, 32, 3)), 
-   "resnet101": (resnet101(), create_resnet101_cifar10, (32, 32, 3)),
-   "resnet152": (resnet152(), create_resnet152_cifar10, (32, 32, 3)),
-   "googlenet": (googlenet(), create_inceptionv3_cifar10, (299, 299, 3))   
+   "vgg11": (vgg11(), create_vgg11, (224, 224, 3), "imagenet"),
+   "vgg16": (vgg16(), create_vgg16, (224, 224, 3), "imagenet"),
+   "vgg19": (vgg19(), create_vgg19_imagenet, (32, 32, 3), "cifar10"),
+   "alexnet": (alexnet(), create_alexnet_cifar10, (32, 32, 3), "cifar10"),
+   "densenet121": (densenet121(), create_densenet121_cifar10, (32, 32, 3), "cifar10"),
+   "densenet169": (densenet169(), create_densenet169_cifar10, (32, 32, 3), "cifar10"),
+   "densenet201": (densenet201(), create_densenet201_cifar10, (32, 32, 3), "cifar10"),
+   "resnet18": (resnet18(), create_resnet18_cifar10, (32, 32, 3), "cifar10"),
+   "resnet34": (resnet34(), create_resnet34_cifar10, (32, 32, 3), "cifar10"),
+   "resnet50": (resnet50(), create_resnet50_cifar10, (32, 32, 3), "cifar10"),
+   "resnet101": (resnet101(), create_resnet101_cifar10, (32, 32, 3), "cifar10"),
+   "resnet152": (resnet152(), create_resnet152_cifar10, (32, 32, 3), "cifar10"),
+   "googlenet": (googlenet(), create_inceptionv3_cifar10, (299, 299, 3), "mnist"),
 }
 
-def main():
-    test = "resnet18"
-    old_model, create_pydtnn_model,shape = dict_test[test]
+def pytorch_inference(model: torch.nn.Module, dataloader, loss_func:torch.nn.modules.loss._Loss, device:torch.device, metrics_list: list) -> None:
 
-    kwargs = dict()
-    if "model_name" not in kwargs:
-        kwargs["model_name"] = None
-    _old_model = PyDTNN_Model(**kwargs)
-    create_pydtnn_model(_old_model)
-    print("PyDTNN version:")
-    _old_model.show()
-
-    print("====================\n====================")
-    print("PyTorch version")
-    print(f"old_model:\n{old_model}")
-    print("-----")
+    outputs_list = list()
+    labels_list = list()
     
+    model.eval()
+    with torch.no_grad():
+
+        for inputs, labels in dataloader:
+            outputs = model(inputs)            
+            outputs = outputs.to(device)
+            outputs_list.extend(outputs)
+            labels_list.extend(labels)
+            for _, metric in metrics_list:
+                metric.update(outputs, labels)
+    
+    for name, metric in metrics_list:
+        metric_result = metric.compute()
+        print(f"{name}: {metric_result:.4f}")
+
+    print("Output | Label")
+    for output, label in zip(outputs_list, labels_list):
+        print(f"{output} | {label}")
+    
+# --- END pytorch_inference --- #
+
+def print_model_reports(model):
+    # Print performance counter report
+    model.perf_counter.print_report()
+    # Print BestOf report
+    if model.enable_best_of:
+        print()
+        BestOf.print_report()
+
+def pydtnn_inference(model: PyDTNN_Model, metrics_list = None, dataset = None) -> None:
+    metrics_list = [f for f in model.metrics.replace(" ", "").split(",")] if metrics_list is None else metrics_list
+    model.evaluate_dataset(dataset, model.batch_size, model.loss_func, metrics_list)
+    print_model_reports(model)
+
+# --- END pytorch_inference --- #
+
+def main():
+    test = "alexnet"
+    pytorch_model, create_pydtnn_model, shape, dataset = dict_test[test]
+    kwargs = dict()
+
+    kwargs["model_name"] = None
+    kwargs["comm"] = None
+    kwargs["mpi_processes"] = 1
+    kwargs["dataset"] = kwargs["dataset_name"] = dataset
+    kwargs["evaluate_only"] = True
+    kwargs["parallel"] = False
+    kwargs["tensor_format"] = "NCHW" # "NCHW" # "NHWC"
+    kwargs["loss_func"] = "categorical_cross_entropy"
+    kwargs["enable_gpu"] = False
+    kwargs["dataset_train_path"] = DATASET_PATH
+    kwargs["dataset_test_path"] = DATASET_PATH
+
+    print("====================")
+    print("== PyDTNN version ==")
+    print("====================")
+
+
+    old_model = PyDTNN_Model(**kwargs)
+    create_pydtnn_model(old_model)
+    print("PyDTNN version:")
+    old_model.show()
+    print("-----\n")
+    
+    print("=====================")
+    print("== PyTorch version ==")
+    print("=====================")
+    print(pytorch_model)
+    print("-----\n")
+
+    print("PyTorch model's forward method:")
+    graph = torch.fx.symbolic_trace(pytorch_model)
+    print(graph.code)
 
     print("-----\n")
 
-    print("old_model's forward function:")
-    graph = torch.fx.symbolic_trace(old_model)
-    print(graph.code)
+    #dataset = get_dataset(old_model)
+    #pydtnn_inference(model=old_model, dataset = dataset)
 
-    print("======\n")
+    print("=======================")
+    print("== Converted version ==")
+    print("=======================")
 
-    new_model = convert_model(model = old_model, input_shape=shape)
+    new_model = convert_model(model = pytorch_model, input_shape=shape, kwargs=kwargs)
     
-    print("\nnew_model:")
     new_model.show()
     print("-----")
     print(f"type(new_model): {type(new_model)}")
+
+    print("======================\n")
+
+    print("=======================")
+    print("== Testing Inference ==")
+    print("=======================")
+
+    dataset = get_dataset(old_model)
+
+    print("dataset:")
+    print(dataset)
+
+    pydtnn_inference(model=old_model, dataset = dataset)
+
+    print("-------------------")
+    print(" PyDTNN's inference")
+    print("-------------------")
+    
+    pydtnn_inference(model=new_model, dataset = dataset)
+
+    print("-------------------")
+    print("Pytorch's inference")
+    print("-------------------")
+
+    match kwargs["loss_func"]:
+        case "categorical_cross_entropy":
+            loss = CrossEntropyLoss
+        case _:
+            loss = None
+            print("Pick another loss")
+            assert False
+    
+    device = torch.device("cpu") if kwargs["enable_gpu"] == False else torch.device("gpu")
+
+    pytorch_inference(model=pytorch_model, dataloader=None, loss_func=loss, device=device, 
+                      metrics_list=[Accuracy, AUROC, AveragePrecision, F1Score])
 
 if __name__ == "__main__":
     main()