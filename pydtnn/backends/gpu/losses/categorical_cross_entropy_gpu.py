4255494bb6f72d4d01e7b6391b8d7af981b3fa94
Miguel Ángel Prosper <mprosper@uji.es>
2025-04-03 14:59:48 +0200

    Fix: match parameter name with use

--- pydtnn/backends/gpu/losses/categorical_cross_entropy_gpu.py
+++ pydtnn/backends/gpu/losses/categorical_cross_entropy_gpu.py
@@ -57,11 +57,11 @@ class CategoricalCrossEntropyGPU(LossGPU, CategoricalCrossEntropy):
         """.replace("T", {np.float32: "float", np.float64: "double"}[self.model.dtype]))
         return module.get_function("categorical_cross_entropy")
 
-    def __call__(self, y_pred, y_targ, global_batch_size):
+    def __call__(self, y_pred, y_targ, batch_size):
         threads = min(self.model.batch_size, 1024)
         blocks = max(self.model.batch_size, 1024) // threads + 1
         self.kernel(y_targ.ary, y_pred.ary, self.loss, self.dx.ary,
-                    np.int32(self.model.batch_size), np.int32(global_batch_size),
+                    np.int32(self.model.batch_size), np.int32(batch_size),
                     np.int32(self.shape[1]), np.float32(self.eps),
                     grid=(blocks, 1, 1), block=(threads, 1, 1),
                     stream=self.model.stream)
