82ffc82b498e78e4153d7621d296d7856523d70f
Miguel Ángel Prosper <mprosper@uji.es>
2025-04-07 15:10:05 +0200

    Fix: apply rank_weight only on comm (not local)

--- pydtnn/backends/cpu/layers/layer_cpu.py
+++ pydtnn/backends/cpu/layers/layer_cpu.py
@@ -43,8 +43,8 @@ class LayerCPU(Layer, ABC):
         for w_, dw_ in self.grad_vars.items():
             dw_ = dw_ if gradient else w_
             dw = getattr(self, dw_)
-            dw *= self.model.rank_weight
             dw /= self.model.comm_size
+            dw *= self.model.rank_weight
             req = self.model.comm.Iallreduce(MPI.IN_PLACE, dw, op=MPI.SUM)
             self.reqs_allred[dw_] = req
 
@@ -64,9 +64,9 @@ class LayerCPU(Layer, ABC):
                                           [self.id * PYDTNN_MDL_EVENTS + PYDTNN_MDL_ALLREDUCE_DW,
                                            self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_ALLREDUCE_DW])
             dw = getattr(self, dw_)
-            dw *= self.model.rank_weight
             dw /= self.model.comm_size
             if comm:
+                dw *= self.model.rank_weight
                 self.model.comm.Allreduce(MPI.IN_PLACE, dw, op=MPI.SUM)
             else:
                 dw *= self.model.comm_size
