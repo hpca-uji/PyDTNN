0a8d5ce6350c484862a9fd7a50ff11e14dfe4104
Paul <al386152@uji.es>
2025-04-07 11:43:49 +0200

    Added supporting for int8 and float64

--- pydtnn/cython_modules/oversampling_nchw_cython.pyx
+++ pydtnn/cython_modules/oversampling_nchw_cython.pyx
@@ -24,25 +24,30 @@ from cython.parallel import prange
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-# extra_h = new_h // h, new_h >= h ; extra_w = new_w // w, new_w >= w;
-def oversampling_fwd_nchw_cython(np.ndarray x, int new_h, int new_w, 
-                                int extra_h, int extra_w) -> np.ndarray:
-    cdef int n = x.shape[0]
-    cdef int c = x.shape[1]
-    cdef int h = x.shape[2]
-    cdef int w = x.shape[3]
+def _oversampling_int_8(np.ndarray[np.int8_t, ndim=4] oversampled_x,
+                            np.ndarray[np.int8_t, ndim=4] x,
+                            int n, int c, int h, int w, int extra_h, int extra_w) -> np.ndarray:
+    cdef int row, column
+    cdef int nn, cc, hi, hj, i, j
 
-    cdef np.ndarray oversampled_x = np.empty((n, c, new_h, new_w), dtype = x.dtype)
-    
-    return _oversampling_float_32(oversampled_x, x, n, c, h, w, extra_h, extra_w)
-# -- END oversampling_nchw_cython -- #
+    for nn in prange(n, nogil=True):
+        for cc in range(c):
+            for hi in range(h):
+                for hj in range(w):
+                    for i in range(extra_h):
+                        for j in range(extra_w):
+                            row = i + (hi * extra_h)
+                            column = j + (hj * extra_w)
+                            oversampled_x[nn, cc, row, column] = x[nn, cc, hi, hj]
+
+    return oversampled_x
+# --- END _oversampling_float_8 --- #
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def _oversampling_float_32(np.ndarray[np.float32_t, ndim=4] oversampled_x,
                             np.ndarray[np.float32_t, ndim=4] x,
                             int n, int c, int h, int w, int extra_h, int extra_w) -> np.ndarray:
-    
     cdef int row, column
     cdef int nn, cc, hi, hj, i, j
     
@@ -55,8 +60,54 @@ def _oversampling_float_32(np.ndarray[np.float32_t, ndim=4] oversampled_x,
                             row = i + (hi * extra_h)
                             column = j + (hj * extra_w)
                             oversampled_x[nn, cc, row, column] = x[nn, cc, hi, hj]
-
     return oversampled_x
+# --- END _oversampling_float_32 --- #
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def _oversampling_float_64(np.ndarray[np.float64_t, ndim=4] oversampled_x,
+                            np.ndarray[np.float64_t, ndim=4] x,
+                            int n, int c, int h, int w, int extra_h, int extra_w) -> np.ndarray:
+    cdef int row, column
+    cdef int nn, cc, hi, hj, i, j
+    
+    for nn in prange(n, nogil=True):
+        for cc in range(c):
+            for hi in range(h):
+                for hj in range(w):
+                    for i in range(extra_h):
+                        for j in range(extra_w):
+                            row = i + (hi * extra_h)
+                            column = j + (hj * extra_w)
+                            oversampled_x[nn, cc, row, column] = x[nn, cc, hi, hj]
+    return oversampled_x
+# --- END _oversampling_float_64 --- #
+
+
+PSEUDO_SWITCH_FUNCTIONS = {    
+    np.dtype('int8'): _oversampling_int_8,
+    np.dtype('float32'): _oversampling_float_32,
+    np.dtype('float64'): _oversampling_float_64,
+}
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+# extra_h = new_h // h, new_h >= h ; extra_w = new_w // w, new_w >= w;
+def oversampling_fwd_nchw_cython(np.ndarray x, int new_h, int new_w, 
+                                int extra_h, int extra_w) -> np.ndarray:
+    cdef int n = x.shape[0]
+    cdef int c = x.shape[1]
+    cdef int h = x.shape[2]
+    cdef int w = x.shape[3]
+    cdef dict pseudo_switch = PSEUDO_SWITCH_FUNCTIONS
+
+    cdef np.ndarray oversampled_x = np.empty((n, c, new_h, new_w), dtype = x.dtype)
+
+    if x.dtype in PSEUDO_SWITCH_FUNCTIONS:
+        return pseudo_switch[x.dtype](oversampled_x, x, n, c, h, w, extra_h, extra_w)
+    else: 
+        raise TypeError(f" Type received '{x.dtype}'. Types expected in oversampling_fwd_nchw_cython: {PSEUDO_SWITCH_FUNCTIONS.keys()}")
+# -- END oversampling_nchw_cython -- #
 
 ###########################################################
 ###########################################################
