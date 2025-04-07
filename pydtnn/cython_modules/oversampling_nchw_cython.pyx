71a009c0a4dad510c9ec190cf00f5d9dfafc2038
Paul <al386152@uji.es>
2025-04-07 15:01:39 +0200

    Moved the oversampling function to only one function for every supported type of data

--- pydtnn/cython_modules/oversampling_nchw_cython.pyx
+++ pydtnn/cython_modules/oversampling_nchw_cython.pyx
@@ -22,6 +22,55 @@ cimport numpy as np
 cimport cython
 from cython.parallel import prange
 
+ctypedef fused supported_types_t:
+    np.int8_t
+    np.float32_t
+    np.float64_t
+
+def oversampling(np.ndarray[supported_types_t, ndim=4] oversampled_x, 
+                            np.ndarray[supported_types_t, ndim=4] x, 
+                            int n, int c, int h, int w, int extra_h, int extra_w):
+    cdef int row, column
+    cdef int nn, cc, hi, hj, i, j
+
+    for nn in prange(n, nogil=True):
+        for cc in range(c):
+            for hi in range(h):
+                for hj in range(w):
+                    for i in range(extra_h):
+                        for j in range(extra_w):
+                            row = i + (hi * extra_h)
+                            column = j + (hj * extra_w)
+                            oversampled_x[nn, cc, row, column] = x[nn, cc, hi, hj]
+
+    return oversampled_x
+# --- END oversampling --- #
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+# extra_h = new_h // h, new_h >= h ; extra_w = new_w // w, new_w >= w;
+def oversampling_fwd_nchw_cython(np.ndarray x, int new_h, int new_w, 
+                                int extra_h, int extra_w) -> np.ndarray:
+    cdef int n = x.shape[0]
+    cdef int c = x.shape[1]
+    cdef int h = x.shape[2]
+    cdef int w = x.shape[3]
+
+    cdef np.ndarray oversampled_x = np.empty((n, c, new_h, new_w), dtype = np.float64)
+    cdef x2 = x.astype(np.float64)
+
+    try:
+        return oversampling(oversampled_x, x2, n, c, h, w, extra_h, extra_w)
+    except TypeError:
+        raise TypeError(f"Type '{x2.dtype}' is not supported by oversampling_fwd_nchw_cython")
+# --- END oversampling_fwd_nchw_cython --- #
+
+###########################################################
+###########################################################
+###########################################################
+
+# Version with different functions for every type of data.
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def _oversampling_int_8(np.ndarray[np.int8_t, ndim=4] oversampled_x,
@@ -83,30 +132,27 @@ def _oversampling_float_64(np.ndarray[np.float64_t, ndim=4] oversampled_x,
     return oversampled_x
 # --- END _oversampling_float_64 --- #
 
-
-PSEUDO_SWITCH_FUNCTIONS = {    
-    np.dtype('int8'): _oversampling_int_8,
-    np.dtype('float32'): _oversampling_float_32,
-    np.dtype('float64'): _oversampling_float_64,
-}
-
 @cython.boundscheck(False)
 @cython.wraparound(False)
 # extra_h = new_h // h, new_h >= h ; extra_w = new_w // w, new_w >= w;
-def oversampling_fwd_nchw_cython(np.ndarray x, int new_h, int new_w, 
+def _oversampling_fwd_nchw_cython(np.ndarray x, int new_h, int new_w, 
                                 int extra_h, int extra_w) -> np.ndarray:
     cdef int n = x.shape[0]
     cdef int c = x.shape[1]
     cdef int h = x.shape[2]
     cdef int w = x.shape[3]
-    cdef dict pseudo_switch = PSEUDO_SWITCH_FUNCTIONS
+    cdef dict pseudo_switch = {    
+        np.dtype('int8'): _oversampling_int_8,
+        np.dtype('float32'): _oversampling_float_32,
+        np.dtype('float64'): _oversampling_float_64,
+    }
 
     cdef np.ndarray oversampled_x = np.empty((n, c, new_h, new_w), dtype = x.dtype)
 
-    if x.dtype in PSEUDO_SWITCH_FUNCTIONS:
+    if x.dtype in pseudo_switch:
         return pseudo_switch[x.dtype](oversampled_x, x, n, c, h, w, extra_h, extra_w)
     else: 
-        raise TypeError(f" Type received '{x.dtype}'. Types expected in oversampling_fwd_nchw_cython: {PSEUDO_SWITCH_FUNCTIONS.keys()}")
+        raise TypeError(f" Type received '{x.dtype}'. Types expected in oversampling_fwd_nchw_cython: {pseudo_switch.keys()}")
 # -- END oversampling_nchw_cython -- #
 
 ###########################################################
