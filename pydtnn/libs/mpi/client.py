0d0659c41613ac0ee8402efe27c0ff464579ffab
Miguel Ángel Prosper <mprosper@uji.es>
2025-05-23 08:06:40 +0200

    Fix: ensure request is in state before sending

--- pydtnn/libs/mpi/client.py
+++ pydtnn/libs/mpi/client.py
@@ -158,8 +158,8 @@ class Comm:
             raise RuntimeError("Tried to schedule operation without participating")
 
         request = Request()
-        self._comm.put(operation)
         self._requests[operation.id] = request
+        self._comm.put(operation)
         self._pool.submit(self._recive_response).add_done_callback(lambda future: future.result())
         self._pool.submit(self._recive_response).add_done_callback(lambda future: future.result())
         return request
