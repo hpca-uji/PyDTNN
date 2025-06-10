a0163449a8a6f0305d12bb693febfa3e5c43ef65
Paul <al386152@uji.es>
2025-06-10 13:19:31 +0200

    Removed duplicated Input layer in SimpleCNN

--- pydtnn/models/simplecnn.py
+++ pydtnn/models/simplecnn.py
@@ -28,7 +28,6 @@ def create_simplecnn(input_shape: Sequence[int], output_shape: Sequence[int]) ->
     _ = model.append
 
     _(Input(shape=input_shape))
-    _(Input(shape=(28, 28, 1)))
     _(Conv2D(nfilters=4, filter_shape=(3, 3), padding=1, stride=1, activation="relu"))
     _(Conv2D(nfilters=8, filter_shape=(3, 3), padding=1, stride=1, activation="relu"))
     _(MaxPool2D(pool_shape=(2, 2), stride=2))
