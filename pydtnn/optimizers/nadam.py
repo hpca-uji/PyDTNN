7d5934bae13562e0d90f07f22e38cf069b54e8d6
Paul <al386152@uji.es>
2025-06-10 12:06:09 +0200

    Small changes in the Optimizers to do a Cherry-pick into develop and small optimization in SGD_CPU

--- pydtnn/optimizers/nadam.py
+++ pydtnn/optimizers/nadam.py
@@ -29,7 +29,7 @@ class Nadam(Optimizer, ABC):
     Nadam optimizer
     """
 
-    def __init__(self, learning_rate:float=1e-2, beta1:float=0.99, beta2:float=0.999, 
+    def __init__(self, learning_rate:float=1e-2, beta1:float=0.99, beta2:float=0.999,
                  epsilon:float=1e-7, decay:float=0.0, dtype:np.dtype=np.float32):
         super().__init__(learning_rate=learning_rate, dtype=dtype)
         self.beta1 = beta1
--- pydtnn/optimizers/rmsprop.py
+++ pydtnn/optimizers/rmsprop.py
@@ -29,7 +29,7 @@ class RMSProp(Optimizer, ABC):
     RMSProp optimizer
     """
 
-    def __init__(self, learning_rate:float=1e-2, rho:float=0.9, epsilon:float=1e-7, 
+    def __init__(self, learning_rate:float=1e-2, rho:float=0.9, epsilon:float=1e-7,
                  decay:float=0.0, dtype:np.dtype=np.float32):
         super().__init__(learning_rate=learning_rate, dtype=dtype)
         self.rho = rho
