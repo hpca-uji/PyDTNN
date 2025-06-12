5fc0bb81e26fe7c71b12be365a05bd418d1898cd
Paul <al386152@uji.es>
2025-06-12 14:33:06 +0200

    Added Typing. Moved Constats to an Enum

--- pydtnn/backends/cpu/layers/conv_2d_variants/best_of_variant.py
+++ pydtnn/backends/cpu/layers/conv_2d_variants/best_of_variant.py
@@ -25,7 +25,17 @@ from pydtnn.backends.cpu.layers.conv_2d_variants.conv_winograd_variant import Co
 from pydtnn.model import TRAIN_MODE, EVALUATE_MODE
 from pydtnn.utils.best_of import BestOf
 
+from enum import StrEnum, auto
 
+class ConvVariantEnum(StrEnum):
+    BEST_OF = auto()
+    I2C = auto()
+    POINTWISE = auto()
+    DEPTHWISE = auto()
+    # The following values are not set by auto due it's necessary that have that value.
+    GEMM = "cg"
+    WINOGRAD = "cw"
+    DIRECT = "cd0"
 class BestOfVariant(ConvWinogradVariant, ConvDirectVariant, ABC):
 
     def __init__(self, *args, **kwargs):
@@ -36,11 +46,11 @@ class BestOfVariant(ConvWinogradVariant, ConvDirectVariant, ABC):
         # Other parameters
         self.variant = None
 
-    def initialize(self, prev_shape, need_dx=True):
+    def initialize(self, prev_shape: tuple[int,...], need_dx=True):
         super().initialize(prev_shape, need_dx)
         if self.model.enable_best_of:
             # Set variant to 'best_of' and set alternatives to only forward, and forward backward best_ofs
-            self.variant = 'best_of'
+            self.variant = ConvVariantEnum.BEST_OF()
             # Bestof will honor the next configuration options:
             # - enable_conv_winograd
             # - enable_conv_gemm
@@ -50,14 +60,14 @@ class BestOfVariant(ConvWinogradVariant, ConvDirectVariant, ABC):
             alternatives_fw = []
             alternatives_fw_bw_pipeline = []
             if self.model.enable_conv_i2c:
-                alternatives_fw.append(('i2c', self._get_class_forward_and_backward('i2c')[0]))
-                alternatives_fw_bw_pipeline.append(('i2c', self._get_class_forward_and_backward('i2c')))
+                alternatives_fw.append((ConvVariantEnum.I2C, self._get_class_forward_and_backward(ConvVariantEnum.I2C)[0]))
+                alternatives_fw_bw_pipeline.append((ConvVariantEnum.I2C, self._get_class_forward_and_backward(ConvVariantEnum.I2C)))
             if self.model.enable_conv_gemm:
-                alternatives_fw.append(('cg', self._get_class_forward_and_backward('cg')[0]))
-                alternatives_fw_bw_pipeline.append(('cg', self._get_class_forward_and_backward('cg')))
+                alternatives_fw.append((ConvVariantEnum.GEMM, self._get_class_forward_and_backward(ConvVariantEnum.GEMM)[0]))
+                alternatives_fw_bw_pipeline.append((ConvVariantEnum.GEMM, self._get_class_forward_and_backward(ConvVariantEnum.GEMM)))
             if self.model.enable_conv_winograd and self.cw_constraints_fulfilled:
-                alternatives_fw.append(('cw', self._get_class_forward_and_backward('cw')[0]))
-                alternatives_fw_bw_pipeline.append(('cw', self._get_class_forward_and_backward('cw')))
+                alternatives_fw.append((ConvVariantEnum.WINOGRAD, self._get_class_forward_and_backward(ConvVariantEnum.WINOGRAD)[0]))
+                alternatives_fw_bw_pipeline.append((ConvVariantEnum.WINOGRAD, self._get_class_forward_and_backward(ConvVariantEnum.WINOGRAD)))
             if self.model.enable_conv_direct:
                 for n in range(len(self.cd)):
                     cdn = f"cd{n}"
