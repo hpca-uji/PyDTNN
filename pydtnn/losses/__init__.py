c54ea44f6ad1b57894f9f36023b228abae1d1b03
Paul <al386152@uji.es>
2025-06-17 14:20:17 +0200

    Now there is a match-case to select the Loss function

--- pydtnn/losses/__init__.py
+++ pydtnn/losses/__init__.py
@@ -37,3 +37,19 @@ binary_cross_entropy = BinaryCrossEntropy
 
 # Search this module for Loss derived classes and expose them
 get_derived_classes(Loss, locals())
+
+def switch_losses(loss_func_name: str) -> Loss:
+    # From snake to camel, if it's necessary
+    _loss_func_name = loss_func_name.split("_")
+    if len(_loss_func_name) > 1:
+        _loss_func_name = "".join(map(lambda x: x.lower().capitalize(), _loss_func_name))
+    else:
+        _loss_func_name = loss_func_name
+
+    match _loss_func_name:
+        case CategoricalCrossEntropy.__name__:
+            return categorical_cross_entropy
+        case BinaryCrossEntropy.__name__:
+            return binary_cross_entropy
+        case _:
+            raise NotImplementedError(f"\'{loss_func_name}\' is not implemented!")
