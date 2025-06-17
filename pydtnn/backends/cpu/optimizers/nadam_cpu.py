10624e2db70a3f485ad730b6631a9a20653dec1d
Paul <al386152@uji.es>
2025-06-17 15:06:10 +0200

    Adam CPU now works as expected. Added extra typing in CPU's adam and nadam.

--- pydtnn/backends/cpu/optimizers/nadam_cpu.py
+++ pydtnn/backends/cpu/optimizers/nadam_cpu.py
@@ -43,10 +43,12 @@ class NadamCPU(OptimizerCPU, Nadam):
 
     def update(self, layer: LayerCPU) -> None:
         self.context[layer]["it"] += 1
-        it = self.context[layer]["it"]
+        it:int = self.context[layer]["it"]
 
         for w_, dw_ in layer.grad_vars.items():
             w, dw = getattr(layer, w_), getattr(layer, dw_)
+            w: np.ndarray
+            dw: np.ndarray
             # Momentum of the weight or bias of the given layer
             m:np.ndarray = self.context[layer]["m_%s" % w_]
             # Velocity of the weight or bias of the given layer
