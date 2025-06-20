2794857746264736f9633372282a314c01ea9355
Paul <al386152@uji.es>
2025-06-20 10:44:51 +0200

    Fixed small changes that broke the code.

--- pydtnn/backends/cpu/layers/addition_block_cpu.py
+++ pydtnn/backends/cpu/layers/addition_block_cpu.py
@@ -33,23 +33,40 @@ class AdditionBlockCPU(AbstractBlockLayerCPU, AdditionBlock):
         self.shape = self.out_shapes[0]
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        sum_forwards = np.zeros_like(x, dtype=x.dtype)
-        for p in self.paths:
+        
+        num_paths = len(self.paths)
+        p = self.paths[0]
+        x_forward = x
+        for layer in p:
+            self.model.tracer.emit_event(PYDTNN_MDL_EVENT, layer.id * PYDTNN_MDL_EVENTS + PYDTNN_MDL_EVENT_enum.FORWARD)
+            x_forward = layer.forward(x_forward)
+            self.model.tracer.emit_event(PYDTNN_MDL_EVENT, PYDTNN_EVENT_FINISHED)
+        sum_forwards = x_forward
+
+        for i in range(1, num_paths):
+            p = self.paths[i]
             x_forward = x
             for layer in p:
                 self.model.tracer.emit_event(PYDTNN_MDL_EVENT, layer.id * PYDTNN_MDL_EVENTS + PYDTNN_MDL_EVENT_enum.FORWARD)
                 x_forward = layer.forward(x_forward)
                 self.model.tracer.emit_event(PYDTNN_MDL_EVENT, PYDTNN_EVENT_FINISHED)
-            
             self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.FORWARD_ELTW_SUM)
             eltw_sum_cython(sum_forwards, x_forward)
             self.model.tracer.emit_event(PYDTNN_OPS_EVENT, PYDTNN_EVENT_FINISHED)            
         return sum_forwards
 
     def backward(self, dy:np.ndarray) -> np.ndarray:
-        dx = np.zeros_like(dy, dtype=dy.dtype)
+        num_paths = len(self.paths)
+        p = self.paths[0]
+        dx_backward = dy
+        for layer in reversed(p):
+            self.model.tracer.emit_event(PYDTNN_MDL_EVENT, layer.id * PYDTNN_MDL_EVENTS + PYDTNN_MDL_EVENT_enum.FORWARD)
+            dx_backward = layer.backward(dx_backward)
+            self.model.tracer.emit_event(PYDTNN_MDL_EVENT, PYDTNN_EVENT_FINISHED)
+        dx = dx_backward
 
-        for p in self.paths:
+        for i in range(1, num_paths):
+            p = self.paths[i]
             dx_backward = dy
             for layer in reversed(p):
                 self.model.tracer.emit_event(PYDTNN_MDL_EVENT, layer.id * PYDTNN_MDL_EVENTS + PYDTNN_MDL_EVENT_enum.BACKWARD)
