62beceb9a22be50f438d7c67ac98f25622827470
Paul <al386152@uji.es>
2025-07-10 14:06:28 +0200

    Small improvements in relu implementations.

--- pydtnn/backends/cpu/activations/relu_cpu.py
+++ pydtnn/backends/cpu/activations/relu_cpu.py
@@ -30,11 +30,12 @@ class ReluCPU(ActivationCPU, Relu):
 
     def initialize(self, prev_shape):
         super().initialize(prev_shape)
+        self._y = np.empty((self.model.batch_size, *self.prev_shape), dtype=self.model.dtype)
+        self._mask = np.empty((self.model.batch_size, *self.prev_shape), dtype=np.int8)
 
     def forward(self, x:np.ndarray) -> np.ndarray:
-        self.y = np.empty((x.shape[0], *self.prev_shape), dtype=self.model.dtype)
-        self.mask = np.empty((x.shape[0], *self.prev_shape), dtype=np.int8)
-        
+        self.y = self._y[:x.shape[0], :]
+        self.mask = self._mask[:x.shape[0], :]
         relu_cython(x.reshape(-1, copy=False), self.y.reshape(-1, copy=False), self.mask.reshape(-1, copy=False))
         return self.y
 
