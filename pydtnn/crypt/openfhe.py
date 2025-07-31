e24057de9aafeddf5debdb78f236b47424cfd595
Miguel Ángel Prosper <miguelangel.prosper@gmail.com>
2025-07-31 13:18:45 +0200

    crypt: generic contexts

--- pydtnn/crypt/openfhe.py
+++ pydtnn/crypt/openfhe.py
@@ -1,11 +1,7 @@
 """OpenFHE encryption"""
 
 import sys
-import math
 import copyreg
-import operator
-import itertools
-from collections import abc
 from dataclasses import dataclass
 
 # Make sure global package is not confused with current package
@@ -17,6 +13,8 @@ finally:
 
 import numpy as np
 
+from pydtnn import crypt
+
 
 __all__ = (
     "Context",
@@ -24,39 +22,17 @@ __all__ = (
 
 
 @dataclass(repr=False, eq=False, order=False, slots=True, frozen=True)
-class Ciphertext:
+class Ciphertext[P: np.number](crypt.Ciphertext[openfhe.Ciphertext, P]):
     """OpenFHE ciphertext"""
-    _type: np.number
-    _shape: tuple[int, ...]
-    _chunks: tuple[openfhe.Ciphertext, ...]
-
-    def __add__(self, other):
-        """Add two ciphertexts"""
-        if not isinstance(other, Ciphertext):
-            raise NotImplementedError()
-
-        if other._type != self._type:
-            raise TypeError(f"Different underlying types ({other._type} != {self._type})")
-
-        if other._shape != self._shape:
-            raise TypeError(f"Different underlying shapes ({other._shape} != {self._shape})")
-
-        chunks = tuple(itertools.starmap(operator.add, zip(self._chunks, other._chunks)))
-
-        return Ciphertext(
-            _type=self._type,
-            _shape=self._shape,
-            _chunks=chunks
-        )
 
 
-class Context:
+class Context(crypt.Context[openfhe.Ciphertext]):
     """OpenFHE context"""
+    _cls = Ciphertext
 
     def __init__(self):
         """Inizialize context"""
-        self._slots = 4096
-        ring_dim = self._slots * 2
+        ring_dim = self._size * 2
         level = openfhe.SecurityLevel.HEStd_128_classic
 
         # Context
@@ -75,44 +51,17 @@ class Context:
         self._public_key = keys.publicKey
         self._private_key = keys.secretKey
 
-    def _pack(self, obj: np.ndarray) -> abc.Generator[list]:
-        """Transform numpy array into batched lists"""
-        if obj.size == 0:
-            return
-        for part in np.array_split(obj.reshape(-1), range(self._slots, obj.size, self._slots)):
-            yield part.tolist()
-
-    def _encrypt(self, plain: list) -> openfhe.Ciphertext:
+    def _encrypt_chunk(self, chunk: list) -> openfhe.Ciphertext:
         """Encode list to ciphertext"""
-        pack = self._context.MakeCKKSPackedPlaintext(plain)
+        pack = self._context.MakeCKKSPackedPlaintext(chunk)
         cipher = self._context.Encrypt(self._public_key, pack)
         return cipher
 
-    def _decrypt(self, cipher: openfhe.Ciphertext) -> list:
+    def _decrypt_chunk(self, chunk: openfhe.Ciphertext) -> list:
         """Decode cypertext to list"""
-        pack = self._context.Decrypt(cipher, self._private_key)
+        pack = self._context.Decrypt(chunk, self._private_key)
         plain = pack.GetRealPackedValue()
         return plain
-
-    def encrypt(self, obj: np.ndarray) -> Ciphertext:
-        """Encode numpy array to ciphertext"""
-        data = tuple(map(self._encrypt, self._pack(obj)))
-
-        return Ciphertext(
-            _type=obj.dtype.type,
-            _shape=obj.shape,
-            _chunks=data
-        )
-
-    def decrypt(self, obj: Ciphertext) -> np.ndarray:
-        """Decode cypertext to numpy array"""
-        data = itertools.chain.from_iterable(map(self._decrypt, obj._chunks))
-
-        return np.fromiter(
-            iter=data,
-            dtype=obj._type,
-            count=math.prod(obj._shape)
-        ).reshape(obj._shape)
 
 
 # Serialization
--- pydtnn/crypt/tenseal.py
+++ pydtnn/crypt/tenseal.py
@@ -1,12 +1,12 @@
 """TenSEAL encryption"""
 
+# NOTE: Dataclasses with slots can not use zero-arg super()
+# Reference: https://github.com/python/cpython/issues/90562
+
 import sys
-import math
 import pickle
 import copyreg
-import operator
-import itertools
-from collections import abc
+import dataclasses
 from dataclasses import dataclass
 
 # Make sure global package is not confused with current package
@@ -21,47 +21,39 @@ from tenseal import sealapi
 from tenseal.tensors import CKKSVector
 from tenseal.enc_context import Context as SealContext
 
+from pydtnn import crypt
+
 
 __all__ = (
     "Context",
 )
 
 
-@dataclass(repr=False, eq=False, order=False, slots=True, frozen=True)
-class Ciphertext:
+@dataclass(eq=False, order=False, slots=True, frozen=True)
+class Ciphertext[P: np.number](crypt.Ciphertext[CKKSVector, P]):
     """TenSEAL ciphertext"""
-    _type: np.number
-    _shape: tuple[int, ...]
-    _chunks: tuple[CKKSVector, ...]
-    _context: bytes
+    _context: bytes = dataclasses.field(repr=False)
 
-    def __add__(self, other):
-        """Add two ciphertexts"""
-        if not isinstance(other, Ciphertext):
-            raise NotImplementedError()
+    def _new(self, /, *args, **kwds):
+        """Create new operable ciphertext"""
+        return super(Ciphertext, self)._new(_context=self._context, *args, **kwds)
 
-        if other._type != self._type:
-            raise TypeError(f"Different underlying types ({other._type} != {self._type})")
-
-        if other._shape != self._shape:
-            raise TypeError(f"Different underlying shapes ({other._shape} != {self._shape})")
+    def _operable(self, other) -> None:
+        """Ensure ciphertext is operable"""
+        super(Ciphertext, self)._operable(other)
 
         if other._context != self._context:
-            raise TypeError(f"Different underlying contexts ({hash(other._context)} != {hash(self._context)})")
+            raise TypeError(f"Different underlying context ({hash(other._context)} != {hash(self._context)})")
 
-        # Relink contexts
+        # Synchronize contexts
         context = self._get_context() or other._get_context() or self._load_context()
-        for chunk in itertools.chain(self._chunks, other._chunks):
+        self._link_context(context)
+        other._link_context(context)
+
+    def _link_context(self, context: SealContext):
+        """Link all chunks to context"""
+        for chunk in self._chunks:
             chunk.link_context(context)
-
-        chunks = tuple(itertools.starmap(operator.add, zip(self._chunks, other._chunks)))
-
-        return Ciphertext(
-            _type=self._type,
-            _shape=self._shape,
-            _chunks=chunks,
-            _context=self._context
-        )
 
     def _load_context(self) -> SealContext:
         """Load stored context"""
@@ -78,13 +70,13 @@ class Ciphertext:
             return None
 
 
-class Context:
+class Context(crypt.Context[CKKSVector]):
     """TenSEAL context"""
+    _cls = Ciphertext
 
     def __init__(self):
         """Inizialize context"""
-        self._slots = 4096
-        poly_degree = self._slots * 2
+        poly_degree = self._size * 2
         level = sealapi.SEC_LEVEL_TYPE.TC128
 
         # Context
@@ -109,42 +101,18 @@ class Context:
 
         self._context = pickle.dumps(self._public_context)
 
-    def _pack(self, obj: np.ndarray) -> abc.Generator[list]:
-        """Transform numpy array into batched lists"""
-        if obj.size == 0:
-            return
-        for part in np.array_split(obj.reshape(-1), range(self._slots, obj.size, self._slots)):
-            yield part.tolist()
+    def _new(self, /, *args, **kwds) -> crypt.Ciphertext:
+        """Create new operable ciphertext"""
+        return super()._new(_context=self._context, *args, **kwds)
 
-    def _encrypt(self, plain: list) -> CKKSVector:
+    def _encrypt_chunk(self, chunk: list) -> CKKSVector:
         """Encode list to ciphertext"""
-        return tenseal.ckks_vector(self._public_context, plain)
+        return tenseal.ckks_vector(self._public_context, chunk)
 
-    def _decrypt(self, cipher: CKKSVector) -> list:
+    def _decrypt_chunk(self, chunk: CKKSVector) -> list:
         """Decode cypertext to list"""
-        cipher.link_context(self._private_context)
-        return cipher.decrypt()
-
-    def encrypt(self, obj: np.ndarray) -> Ciphertext:
-        """Encode numpy array to ciphertext"""
-        data = tuple(map(self._encrypt, self._pack(obj)))
-
-        return Ciphertext(
-            _type=obj.dtype.type,
-            _shape=obj.shape,
-            _chunks=data,
-            _context=self._context
-        )
-
-    def decrypt(self, obj: Ciphertext) -> np.ndarray:
-        """Decode cypertext to numpy array"""
-        data = itertools.chain.from_iterable(map(self._decrypt, obj._chunks))
-
-        return np.fromiter(
-            iter=data,
-            dtype=obj._type,
-            count=math.prod(obj._shape)
-        ).reshape(obj._shape)
+        chunk.link_context(self._private_context)
+        return chunk.decrypt()
 
 
 # Pickle support
