a7cc3ff6062f022cdc7e143bd850ae71f9482005
Miguel Ángel Prosper <mprosper@uji.es>
2025-10-03 14:34:28 +0200

    update notices

--- pydtnn/converters/imagenet2pydtnn.py
+++ pydtnn/converters/imagenet2pydtnn.py
@@ -1,7 +1,7 @@
 #
 #  This file is part of Python Distributed Training of Neural Networks (PyDTNN)
 #
-#  Copyright (C) 2024-25 Universitat Jaume I
+#  Copyright (C) 2021-25 Universitat Jaume I
 #
 #  PyDTNN is free software: you can redistribute it and/or modify it under the
 #  terms of the GNU General Public License as published by the Free Software
