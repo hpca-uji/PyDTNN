b227dde6fa4f0b754c67fe26f1eb725a66b27834
Miguel Ángel Prosper <mprosper@uji.es>
2025-10-03 14:40:41 +0200

    update notices

--- pydtnn/backends/cpu/layers/batch_normalization_relu_cpu.py
+++ pydtnn/backends/cpu/layers/batch_normalization_relu_cpu.py
@@ -1,22 +1,3 @@
-#
-#  This file is part of Python Distributed Training of Neural Networks (PyDTNN)
-#
-#  Copyright (C) 2021-25 Universitat Jaume I
-#
-#  PyDTNN is free software: you can redistribute it and/or modify it under the
-#  terms of the GNU General Public License as published by the Free Software
-#  Foundation, either version 3 of the License, or (at your option) any later
-#  version.
-#
-#  This program is distributed in the hope that it will be useful, but WITHOUT
-#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-#  License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-
 from pydtnn.backends.cpu.layers import LayerCPU
 from pydtnn.layers import BatchNormalizationRelu
 from pydtnn.cython_modules import bn_relu_inference_cython
