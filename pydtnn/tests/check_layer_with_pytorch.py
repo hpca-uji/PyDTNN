23d3483420ab02a04afa8a465cb7aa765dff859e
Paul <al386152@uji.es>
2025-10-17 13:45:55 +0200

    Updated test

--- pydtnn/tests/check_layer_with_pytorch.py
+++ pydtnn/tests/check_layer_with_pytorch.py
@@ -16,9 +16,9 @@ from pydtnn.backends.gpu import TensorGPU
 
 # setting random seed
 SEED = 1234
-np.random.seed(SEED)
 # ---------
 
+# TODO: Make threshold change proportonally to the number of elements (more elements, less precission)
 
 # ===============
 # Constant values
@@ -41,7 +41,7 @@ KWARGS = {
     "loss_func": "categorical_cross_entropy",
     "enable_gpu": False, # False,#True,
     "omm": None,
-    "dtype": np.float32,
+    "dtype": np.float64,
     "tracing": False,
     "tracer_output": "",
     "batch_size": N
@@ -53,9 +53,11 @@ AVG_POOL_SHAPE = (3, 3)
 AVG_POOL_PADDING = 0
 AVG_POOL_STRIDE = 1
 
-BATCH_NORMALIZATION_EPSILON=1e-05
+BATCH_NORMALIZATION_GAMMA = 1
+BATCH_NORMALIZATION_BETA = 0
+BATCH_NORMALIZATION_EPSILON= 1e-5
 BATCH_NORMALIZATION_MOMENTUM_PYDTNN= 0.9
-BATCH_NORMALIZATION_MOMENTUM_TORCH= 1 - BATCH_NORMALIZATION_MOMENTUM_PYDTNN
+BATCH_NORMALIZATION_MOMENTUM_TORCH= BATCH_NORMALIZATION_MOMENTUM_PYDTNN
 BATCH_NORMALIZATION_NUM_FEATURES = C
 
 CONV2D_IN_C_TORCH = C
@@ -72,6 +74,16 @@ MAX_POOL_SHAPE = (2,2)
 MAX_POOL_PADDING = 0
 MAX_POOL_STRIDE = 1
 MAX_POOL_DILATION = 1
+
+
+GRAD_EQUIVALENCES: dict[str, str] = {
+    "weights" : "weight",
+    "biases" : "bias",
+    # "running_mean": "running_mean", # Not PyDTNN's grading var.
+    # "running_var": "running_var", # Not PyDTNN's grading var.
+    #"beta": "", # Not in PyTorch
+    #"gamma": "", # Not in PyTorch
+}
 # ==============
 
 # ==============
@@ -133,16 +145,6 @@ class TorchConcatenationBlock(torch.nn.Module):
 # -------------
 # ====================
 
-shape = (N, *SHAPE)
-num_elems = np.prod(shape) // 4
-x_1 = np.arange(num_elems)
-x_2 = np.arange(num_elems) * -1
-x_1_1 = np.where(x_1 % 2 == 0, x_1, x_1 / 3)
-x_1_2 = np.where(x_1 % 2 != 0, x_1, x_1)
-x_2_1 = np.where(x_1 % 2 == 0, x_2, x_2 / 3)
-x_2_2 = np.where(x_1 % 2 != 0, x_2, x_2)
-x = np.stack([x_1_1, x_1_2, x_2_1, x_2_2], axis=0, dtype=np.float32, casting="unsafe").reshape(shape)
-
 class CheckLayerWithPyTorch(unittest.TestCase):
 
     # ======================
@@ -151,8 +153,8 @@ class CheckLayerWithPyTorch(unittest.TestCase):
 
     @staticmethod
     def get_test_data() -> np.ndarray:
-        shape = (N, *SHAPE)
-        num_elems = np.prod(shape) // 4
+        shape_with_elements = (N, *SHAPE)
+        num_elems = np.prod(shape_with_elements) // 4
 
         x_1 = np.arange(num_elems)
         x_2 = np.arange(num_elems) * -1
@@ -163,9 +165,11 @@ class CheckLayerWithPyTorch(unittest.TestCase):
         x_2_1 = np.where(x_1 % 2 == 0, x_2, x_2 / 3)
         x_2_2 = np.where(x_1 % 2 != 0, x_2, x_2)
 
-        x = np.stack([x_1_1, x_1_2, x_2_1, x_2_2], axis=0, dtype=np.float32, casting="unsafe").reshape(shape, order="C", copy=None)
+        # NOTE: seems that PyTorch doesn't like too much np.float64
+        x = np.stack([x_1_1, x_1_2, x_2_1, x_2_2], axis=0, dtype=KWARGS["dtype"]).reshape(shape_with_elements)
         np.random.shuffle(x)
 
+        #return np.asarray(x, dtype=KWARGS["dtype"], order="C", copy=True)
         return x
     # ---------
 
@@ -179,17 +183,67 @@ class CheckLayerWithPyTorch(unittest.TestCase):
         return model
     # ---------
 
+    def _copy_grad_vars(self, grad: np.ndarray, grad_var: str, torch_layer: torch.nn.Module) -> None:
+        if grad is not None:
+            torch_grad_var = GRAD_EQUIVALENCES[grad_var]
+            torch_grad = getattr(torch_layer, torch_grad_var)
+            torch_grad.copy_(torch.from_numpy(grad.copy()).to(torch.device("cpu")).float())
+    # ---
 
-    def do_test(self, _x: np.ndarray, pydtnn_model: Model, torch_model: torch.nn.Module, name_test:str) -> None:
+    def copy_grad_vars(self, pydtnn_model: Model, torch_model:torch.nn.Module) -> None:
+        layers = pydtnn_model.get_all_layers().copy()
+
+        if isinstance(layers[0], Input):
+            layers.pop(0)
+        
+        print(f"{layers=}")
+        torch_layers = [module for module in torch_model.modules() if not isinstance(module, torch.nn.Sequential)]
+        print(f"{torch_layers=}")
+        #print(f"{(len(torch_layers) == len(layers))=}")
+
+        with torch.no_grad():
+            for i in range(len(layers)):
+                layer = layers[i]
+                torch_layer = torch_layers[i]
+                match layer:
+                    case BatchNormalization():
+                        running_mean = layer.running_mean
+                        running_var = layer.running_var
+                        if running_mean is not None:
+                            torch_layer.running_mean.copy_(torch.from_numpy(running_mean.copy()).to(torch.device("cpu")).float())
+                        if running_var is not None:
+                            torch_layer.running_var.copy_(torch.from_numpy(running_var.copy()).to(torch.device("cpu")).float())
+                    case FC():
+                        for grad_var in layer.grad_vars.keys():
+                            grad:np.ndarray = getattr(layer, grad_var)
+                            grad = grad if grad_var != "weights" else grad.T
+                            self._copy_grad_vars(grad, grad_var, torch_layer)
+                    case _:
+                        for grad_var in layer.grad_vars.keys():
+                            grad:np.ndarray = getattr(layer, grad_var)
+                            self._copy_grad_vars(grad, grad_var, torch_layer)
+
+            # ----
+
+
+    def do_test(self, _x: np.ndarray, pydtnn_model: Model, torch_model: torch.nn.Module, name_test:str, threshold=1e-6) -> None:
+
+        torch.manual_seed(SEED)
+        np.random.seed(SEED)
+
+        self.copy_grad_vars(pydtnn_model, torch_model)
 
         print(f"====================\nTesting: {name_test}\n====================")
 
         x = np.copy(_x)
 
-        for layer in pydtnn_model.layers:
-            x_pydtnn:np.ndarray = layer.forward(x)
+        x = x.astype(dtype=KWARGS["dtype"], order="C", copy=None)
 
-        x = torch.from_numpy(_x.reshape((N, C, H, W), copy=False)).to(torch.device("cpu"))
+        for layer in pydtnn_model.layers:
+            x:np.ndarray = layer.forward(x)
+        x_pydtnn = x
+
+        x = torch.from_numpy(_x.reshape((N, C, H, W), copy=False)).to(torch.device("cpu")).float()
         x_torch: torch.Tensor = torch_model(x)
         x_torch = np.asarray(x_torch.cpu().detach().numpy(), dtype=pydtnn_model.dtype, order="C", copy=None)
 
@@ -202,7 +256,6 @@ class CheckLayerWithPyTorch(unittest.TestCase):
         print(f"x_pydtnn.min:\t{x_pydtnn.min()}")
         print(f"x_torch.min: \t{x_torch.min()}")
 
-        threshold = 1e-6
         diff = x_pydtnn - x_torch
         print(f"diff all zeros {not diff.any()}")
         print(f"diff below threshold {threshold}: {(diff < threshold).all()}")
@@ -228,7 +281,8 @@ class CheckLayerWithPyTorch(unittest.TestCase):
         torch_model = torch.nn.AdaptiveAvgPool2d(output_size=ADAPTIVE_AVG_POOL_OUTPUT_SIZE)
         pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
         _x = CheckLayerWithPyTorch.get_test_data()
-        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="AdaptiveAveragePool2D")
+
+        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="AdaptiveAveragePool2D", threshold=2e-6)
     # ---------
 
 
@@ -237,35 +291,39 @@ class CheckLayerWithPyTorch(unittest.TestCase):
         torch_model = torch.nn.AvgPool2d(kernel_size=AVG_POOL_SHAPE, padding=AVG_POOL_PADDING, stride=AVG_POOL_STRIDE)
         pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
         _x = CheckLayerWithPyTorch.get_test_data()
-        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="AveragePool2D")
+        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="AveragePool2D", threshold=1e-5)
     # ---------
 
 
     def test_BatchNormalization(self):
-        pydtnn_layers = [BatchNormalization(epsilon=BATCH_NORMALIZATION_EPSILON, momentum=BATCH_NORMALIZATION_MOMENTUM_PYDTNN)]
-        torch_model = torch.nn.BatchNorm2d(BATCH_NORMALIZATION_NUM_FEATURES, eps=BATCH_NORMALIZATION_EPSILON, momentum=BATCH_NORMALIZATION_MOMENTUM_TORCH)
+        pydtnn_layers = [BatchNormalization(gamma=BATCH_NORMALIZATION_GAMMA, beta=BATCH_NORMALIZATION_BETA, epsilon=BATCH_NORMALIZATION_EPSILON, 
+                                            momentum=BATCH_NORMALIZATION_MOMENTUM_PYDTNN)]
+        torch_model = torch.nn.BatchNorm2d(BATCH_NORMALIZATION_NUM_FEATURES, eps=BATCH_NORMALIZATION_EPSILON, momentum=BATCH_NORMALIZATION_MOMENTUM_TORCH, affine=False)
         pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
+
         _x = CheckLayerWithPyTorch.get_test_data()
-        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="BatchNormalization")
+        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="BatchNormalization", threshold=1e-1)
     # ---------
 
 
     def test_Conv2D(self):
         pydtnn_layers = [Conv2D(grouping=Conv2D.Grouping.STANDARD, nfilters=CONV2D_N_FILTERS, filter_shape=CONV2D_FILTER_SHAPE, padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION)]
-        torch_model = torch.nn.Conv2d(in_channels=CONV2D_IN_C_TORCH, out_channels=CONV2D_N_FILTERS, kernel_size=CONV2D_FILTER_SHAPE, padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION)
+        torch_model = torch.nn.Conv2d(in_channels=CONV2D_IN_C_TORCH, out_channels=CONV2D_N_FILTERS, kernel_size=CONV2D_FILTER_SHAPE, padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION)        
         pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
         _x = CheckLayerWithPyTorch.get_test_data()
-        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="Conv2D")
+        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="Conv2D", threshold=1e-5)
     # ---------
 
-    # Dropout makes a random mask ==> can not be compared due both PyTorch and PyDTNN create different masks.
-    #def test_Dropout(self):
-    #    pydtnn_layers = [Dropout()]
-    #    torch_model = torch.nn.Dropout()
-    #    pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
-    #    _x = CheckLayerWithPyTorch.get_test_data()
-    #    self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="Dropout")
-    ## ---------
+    
+    def test_Dropout(self):
+        pydtnn_layers = [Dropout()]
+        torch_model = torch.nn.Dropout()
+        pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
+        _x = CheckLayerWithPyTorch.get_test_data()
+        # Dropout makes a random mask ==> can not be compared due both PyTorch and PyDTNN create different masks ==> always correct.
+        threshold = float("inf")
+        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="Dropout", threshold=threshold)
+    # ---------
 
 
     def test_Flatten(self):
@@ -279,10 +337,10 @@ class CheckLayerWithPyTorch(unittest.TestCase):
 
     def test_FC(self):
         pydtnn_layers = [Flatten(), FC(shape=FC_OUPUT_SHAPE)]
-        torch_model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.LazyLinear(out_features=LINEAR_OUTPUT))
+        torch_model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(in_features=np.prod(SHAPE), out_features=LINEAR_OUTPUT))
         pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
         _x = CheckLayerWithPyTorch.get_test_data()
-        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="FC")
+        self.do_test(_x = _x, pydtnn_model=pydtnn_model, torch_model=torch_model, name_test="FC", threshold=1e-1)
     # ---------
 
 
@@ -296,13 +354,18 @@ class CheckLayerWithPyTorch(unittest.TestCase):
 
 
     def test_AdditionBlock(self):
-        pydtnn_layers = [AdditionBlock(
+        pydtnn_layers = [
+            AdditionBlock(
             [Conv2D(grouping=Conv2D.Grouping.STANDARD, nfilters=CONV2D_N_FILTERS, filter_shape=CONV2D_FILTER_SHAPE, 
                     padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION), 
-             BatchNormalization(epsilon=BATCH_NORMALIZATION_EPSILON, momentum=BATCH_NORMALIZATION_MOMENTUM_PYDTNN)],
+             BatchNormalization(gamma=BATCH_NORMALIZATION_GAMMA, beta=BATCH_NORMALIZATION_BETA, epsilon=BATCH_NORMALIZATION_EPSILON, momentum=BATCH_NORMALIZATION_MOMENTUM_PYDTNN)
+            ],
 
             [Conv2D(grouping=Conv2D.Grouping.STANDARD, nfilters=CONV2D_N_FILTERS, filter_shape=CONV2D_FILTER_SHAPE, 
-                    padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION)])]
+                    padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION)
+            ]
+            )]
+
         torch_model = TorchAdditionBlock()
         pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
         _x = CheckLayerWithPyTorch.get_test_data()
@@ -311,13 +374,17 @@ class CheckLayerWithPyTorch(unittest.TestCase):
 
 
     def test_ConcatenationBlock(self):
-        pydtnn_layers = [ConcatenationBlock(
+        pydtnn_layers = [
+            ConcatenationBlock(
             [Conv2D(grouping=Conv2D.Grouping.STANDARD, nfilters=CONV2D_N_FILTERS, filter_shape=CONV2D_FILTER_SHAPE, 
                     padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION), 
-             BatchNormalization(epsilon=BATCH_NORMALIZATION_EPSILON, momentum=BATCH_NORMALIZATION_MOMENTUM_PYDTNN)],
+             BatchNormalization(gamma=BATCH_NORMALIZATION_GAMMA, beta=BATCH_NORMALIZATION_BETA, epsilon=BATCH_NORMALIZATION_EPSILON, momentum=BATCH_NORMALIZATION_MOMENTUM_PYDTNN)
+            ],
 
             [Conv2D(grouping=Conv2D.Grouping.STANDARD, nfilters=CONV2D_N_FILTERS, filter_shape=CONV2D_FILTER_SHAPE, 
-                    padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION)])]
+                    padding=CONV2D_PADDING, stride=CONV2D_STRIDE, dilation=CONV2D_DILATION)
+            ]
+            )]
         torch_model = TorchConcatenationBlock()
         pydtnn_model = CheckLayerWithPyTorch.initialize_pydtnn_model(pydtnn_layers, kwargs=KWARGS)
         _x = CheckLayerWithPyTorch.get_test_data()
