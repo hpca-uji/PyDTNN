850afa1e9768add1b939ba7517ab3dced09e66d8
Paul <al386152@uji.es>
2025-10-28 14:55:56 +0100

    Fixed small error related to the data type

--- pydtnn/backends/gpu/utils_gpu.py
+++ pydtnn/backends/gpu/utils_gpu.py
@@ -12,8 +12,8 @@ import numpy as np
 
 def matmul_gpu(handle, trans_a, trans_b, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc, dtype):
     try:
-        gemm = {np.float32: cublas.cublasSgemm,
-                np.float64: cublas.cublasDgemm}[dtype]
+        gemm = {np.dtype(np.float32): cublas.cublasSgemm,
+                np.dtype(np.float64): cublas.cublasDgemm}[dtype]
     except KeyError:
         print("I cannot handle %s type!\n" % dtype.__name__)
     else:
@@ -22,8 +22,8 @@ def matmul_gpu(handle, trans_a, trans_b, m, n, k, alpha, a, lda, b, ldb, beta, c
 
 def matvec_gpu(handle, trans_a, m, n, alpha, a, lda, b, ldb, beta, c, ldc, dtype):
     try:
-        gemv = {np.float32: cublas.cublasSgemv,
-                np.float64: cublas.cublasDgemv}[dtype]
+        gemv = {np.dtype(np.float32): cublas.cublasSgemv,
+                np.dtype(np.float64): cublas.cublasDgemv}[dtype]
     except KeyError:
         print("I cannot handle %s type!\n" % dtype.__name__)
     else:
