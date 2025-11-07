5aaa48b563d3d2a744fa4148c35cec168f1fd293
Paul <al386152@uji.es>
2025-11-07 10:09:30 +0100

    Refactor and added some documentation

--- pydtnn/utils/tensor.py
+++ pydtnn/utils/tensor.py
@@ -79,7 +79,16 @@ class TensorFormat(StrEnum):
     def as_sample(self) -> SampleFormat:
         return SampleFormat(self.strip("n"))
 
-    def reshape(self, shape: tuple[int, int, int, int], format: TensorFormat) -> tuple[int, int, int, int]:
+    def reshape(self, shape: tuple[int, int, int, int], dst: TensorFormat) -> tuple[int, int, int, int]:
+        """
+        Reshape \"shape"
+
+        Args:
+            shape (tuple[int, int, int, int]): New shape.
+            dst (TensorFormat): New tensor format.
+        Returns:
+            (tuple[int, int, int, int]): \"shape\" with \"dst\" tensor format.
+        """
         assert len(shape) == len(self), f"Unexpected dimensions (got: {shape}, expect: {self})"
 
         match self:
@@ -90,21 +99,46 @@ class TensorFormat(StrEnum):
             case _:
                 assert_never(self)
 
-        match format:
+        match dst:
             case TensorFormat.NCHW:
                 return (n, c, h, w)
             case TensorFormat.NHWC:
                 return (n, h, w, c)
             case _:
-                assert_never(format)
+                assert_never(dst)
 
-    def transpose(self, data: np.ndarray, format: TensorFormat) -> np.ndarray:
-        return data.transpose(self.reshape(range(len(data.shape)), format))  # type: ignore
+    def transpose(self, data: np.ndarray, dst: TensorFormat) -> np.ndarray:
+        """
+        Transpose elements of \"data\" some it has the \"dst\" tensor format.
+
+        Args:
+            data (np.ndarray): numpy array to transpose to a new tensor format.
+            dst (TensorFormat): The new data's tensor format.
+        Returns:
+            np.ndarray: \"data\" with \"dst\" tensor format.
+        """
+        return data.transpose(self.reshape(range(len(data.shape)), dst))  # type: ignore
 
 
-def encode_tensor(shape: ArrayShape, tensor_format=TensorFormat.NHWC) -> ArrayShape:
-    return SampleFormat.HWC.reshape(shape, tensor_format.as_sample()) if len(shape) == 3 else shape
+def encode_tensor(shape: ArrayShape, dst_format=TensorFormat.NHWC) -> ArrayShape:
+    """
+    Returns the \"shape\" (exepcted in \"NHWC\") in \"dst_format\" format.
+    Args:
+        shape (ArrayShape): shape in \"HWC\" format.
+        dst_format (TensorFormat): The new shape's tensor format. Default: \"TensorFormat.NHWC\".
+    Returns:
+        np.ndarray: \"shape\" with \"dst_format\" tensor format.
+    """
+    return SampleFormat.HWC.reshape(shape, dst_format.as_sample()) if len(shape) == 3 else shape
 
 
-def decode_tensor(shape: ArrayShape, tensor_format=TensorFormat.NHWC) -> ArrayShape:
-    return tensor_format.as_sample().reshape(shape, SampleFormat.HWC) if len(shape) == 3 else shape
+def decode_tensor(shape: ArrayShape, base_format=TensorFormat.NHWC) -> ArrayShape:
+    """
+    Returns the \"shape\" (exepcted in \"base_format\") in \"HWC\" format.
+    Args:
+        shape (ArrayShape): shape in \"base_format\" format.
+        dst_format (TensorFormat): The base format. Default: \"TensorFormat.NHWC\".
+    Returns:
+        np.ndarray: \"shape\" with \"dst_format\" tensor format.
+    """
+    return base_format.as_sample().reshape(shape, SampleFormat.HWC) if len(shape) == 3 else shape
