8e4a2b6104ec18264e9e78434f87b319640579bb
Miguel Ángel Prosper <mprosper@uji.es>
2025-12-09 10:40:13 +0100

    fix script model

--- scripts/run_simplecnn_mnist.sh
+++ scripts/run_simplecnn_mnist.sh
@@ -5,7 +5,7 @@ export PYTHONOPTIMIZE=2
 export PYTHONUNBUFFERED="True"
 
 pydtnn-benchmark \
-  --model=resnet \
+  --model=simplecnn \
   --dataset=mnist \
   --dataset-path=datasets/mnist \
   --test-as-validation=False \
