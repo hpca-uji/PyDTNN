1804a57f718b9540428504d3c543d52d7e36b90c
Paul <pluijter@uji.es>
2026-01-27 14:13:02 +0100

    Reduced i2c's memory use around a 9 %

--- pydtnn/backends/cpu/layers/conv_2d_variants/i2c.py
+++ pydtnn/backends/cpu/layers/conv_2d_variants/i2c.py
@@ -13,73 +13,86 @@ class Conv2DI2CCPU(Conv2DStandardCPU):
     def initialize(self, prev_shape: ArrayShape, x: np.ndarray | None = None) -> None:
         super().initialize(prev_shape, x)
 
-        # self.dim_n: Dimension where the "n" of NCHW/NHWC is used in the calculations.
+        # dim_n: Dimension where the "n" of NCHW/NHWC is used in the calculations.
         # self.dim_c: Dimension where the "c" of NCHW/NHWC is used in the calculations.
-        self.dim_n = self.model.batch_size * self.ho * self.wo
+        dim_n = self.model.batch_size * self.ho * self.wo
         self.dim_c = self.ci * self.kh * self.kw
 
         match self.model.tensor_format:
             case TensorFormat.NCHW:
                 self.forward = self._forward_i2c_nchw
                 self.backward = self._backward_i2c_nchw
-                self._x_cols = np.zeros(shape=(self.dim_c, self.dim_n), dtype=self.model.dtype, order="C")
-
+                self._x_cr_shape = (self.dim_c, dim_n)
                 _dw_shape = (self.co, self.dim_c)
-                self.temp_bw_shape = (self.dim_c, self.dim_n)
-                dx_shape = (self.model.batch_size, self.ci, self.hi, self.wi)
-
-                self.real_memory_size += self._x_cols.size
             case TensorFormat.NHWC:
                 self.forward = self._forward_i2c_nhwc
                 self.backward = self._backward_i2c_nhwc
-                self._x_rows = np.zeros(shape=(self.dim_n, self.dim_c), dtype=self.model.dtype, order="C")                
-
+                self._x_cr_shape = (dim_n, self.dim_c)
                 _dw_shape = (self.dim_c, self.co)
-                self.temp_bw_shape = (self.dim_n, self.dim_c)
-                dx_shape = (self.model.batch_size, self.hi, self.wi, self.ci)
-
-                self.real_memory_size += self._x_rows.size
             case _:
+                self._x_cr_shape = (None, )
                 _dw_shape = (None, )
-                self.temp_bw_shape: tuple[int, ...] = (None, )
-                dx_shape = (None,)
-
                 raise NotImplementedError(f"\"{self.model.tensor_format}\" format not implemented.")
         # -
+        
+        self.temp_bw_shape = self._x_cr_shape
+        self.temp_bw_shape_size = np.prod(self.temp_bw_shape)
 
-        # NOTE: These attributes only store data, their values before the operation doesn't matter; they're initalized due avoid warnings in "LayerAndActivationBase.export".
-        self.y = np.zeros(shape=(self.dim_n, self.co), dtype=self.model.dtype, order="C")
-        self.real_memory_size += self.y.size
+        y_shape = (dim_n, self.co)
+        self.y_size = np.prod(y_shape)
+
+        #self.y = np.zeros(shape=(self.dim_n, self.co), dtype=self.model.dtype, order="C")
+        #self.real_memory_size += self.y.size
 
         if not self.model.evaluate_only:
+            self.dx_shape = self.model.encode_shape((self.model.batch_size, self.ci, self.hi, self.wi))
             self._dw_shape = _dw_shape  # This shape is only for a intermediate operation.
+        else:
+            self.dx_shape = (0,)
+        
+        self.dx_shape_size = int(np.prod(self.dx_shape))
+        # NOTE: These attributes only store data, their values before the operation doesn't matter; they're initalized due avoid warnings in "LayerAndActivationBase.export".
+        self.temp_c_r_dx = np.zeros(shape=(max(np.prod(self._x_cr_shape), self.dx_shape_size), ), dtype=self.model.dtype, order="C")
+        # self.temp_c_r_dx: Temporal array where the cols/rows and dx values are stored.
+        self.real_memory_size += self.temp_c_r_dx.size
 
-            self.dx = np.zeros(shape=dx_shape, dtype=self.model.dtype, order="C")
-            if not self.model.use_memory_pool:
-                self.temp_bw: np.ndarray = np.zeros(shape=self.temp_bw_shape, dtype=self.model.dtype, order="C")
-            else:
-                self.temp_bw: np.ndarray = None  # type: ignore (It will be initialized later)
+        self.temp_y_bc_br = np.zeros(shape=(max(self.y_size, self.temp_bw_shape_size), ), dtype=self.model.dtype, order="C") 
+        # self.temp_y_bc_br: Temporal array where the y and backward's cols/rows values are stored.
+        self.real_memory_size += self.temp_y_bc_br.size
 
-            self.temp_memory_size += int(np.prod(self.temp_bw_shape))
-            self.real_memory_size += self.dx.size
         self.real_memory_size += self.temp_memory_size
     # ---
 
-    def post_initialize(self) -> None:
-        super().post_initialize()
-        if not self.model.evaluate_only:
-            self.temp_bw = self.model.memory_pool.get_ndarray(self.temp_bw_shape)
-            self.model.memory_pool.free_memory(self.temp_memory_size)
-    # ----
+    def get_rows(self, batch_size:int) -> np.ndarray:
+        dim_n = batch_size * self.ho * self.wo
+        shape = (dim_n, self.dim_c)
+        x_rows: np.ndarray = self.temp_c_r_dx[:np.prod(shape)]
+        x_rows = x_rows.reshape(shape)
+        return x_rows 
+
+    def get_cols(self, batch_size:int) -> np.ndarray:
+        dim_n = batch_size * self.ho * self.wo
+        shape = (self.dim_c, dim_n)
+        x_cols: np.ndarray = self.temp_c_r_dx[:np.prod(shape)]
+        x_cols = x_cols.reshape(shape)
+        return x_cols
+    
+    def get_y(self, batch_size:int) -> np.ndarray:
+        dim_n = batch_size * self.ho * self.wo
+        shape = (dim_n, self.co) 
+        y:np.ndarray = self.temp_y_bc_br[:np.prod(shape)]
+        y = y.reshape(shape)
+        return y
 
     def _forward_i2c_nhwc(self, x: np.ndarray) -> np.ndarray:
         """Version of the forward function that uses im2col and matmul"""
 
-        dim_n = x.shape[0] * self.ho * self.wo
         # x_rows = np.zeros(shape=(dim_n, self.dim_c), dtype=self.model.dtype)
         #x_rows = np.asarray(self._x_rows[:dim_n, :], dtype=self.model.dtype, order="C", copy=None)
-        x_rows: np.ndarray = self._x_rows[:dim_n, :]
-        y: np.ndarray = self.y[:dim_n, :]
+        x_rows = self.get_rows(x.shape[0])
+        x_rows.fill(0)
+        #y = self.y[:shape[-1], :]
+        y = self.get_y(x.shape[0])
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.FORWARD_IM2COL)
         im2row_nhwc_cython(x, x_rows,
@@ -114,10 +127,12 @@ class Conv2DI2CCPU(Conv2DStandardCPU):
     def _forward_i2c_nchw(self, x: np.ndarray) -> np.ndarray:
         """Version of the forward function that uses im2col and matmul"""
 
-        dim_n = x.shape[0] * self.ho * self.wo
         # x_cols = np.zeros(shape=(self.dim_c, dim_n), dtype=self.model.dtype)
-        x_cols: np.ndarray = np.asarray(self._x_cols[:, :dim_n], dtype=self.model.dtype, order="C", copy=None)
-        y = self.y[:dim_n, :]
+        # x_cols: np.ndarray = np.asarray(self._x_cr[:, :dim_n], dtype=self.model.dtype, order="C", copy=None)
+        x_cols = self.get_cols(x.shape[0])
+        x_cols.fill(0)
+        #y = self.y[:shape[-1], :]
+        y = self.get_y(x.shape[0])
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.FORWARD_IM2COL)
         im2col_nchw_cython(x, x_cols,
@@ -154,11 +169,8 @@ class Conv2DI2CCPU(Conv2DStandardCPU):
         """Version of the backward function that uses im2col and matmul"""
 
         #res = np.asarray(self.res_bw[:(dy.shape[0] * self.ho * self.wo), :], dtype=self.model.dtype, order="C", copy=None)
-        rows:np.ndarray = self.temp_bw[:(dy.shape[0] * self.ho * self.wo), :]
+        rows:np.ndarray = self.get_rows(dy.shape[0])
         self.dw = self.dw.reshape(self._dw_shape)
-        
-        dx: np.ndarray = self.dx[:dy.shape[0], :]
-        dx.fill(0)  # NOTE: It is necessary that dx is filled with 0s.
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.BACKWARD_TRANSPOSE_DY)
         dy_cols: np.ndarray = dy.reshape((-1, self.co), copy=False)
@@ -191,6 +203,9 @@ class Conv2DI2CCPU(Conv2DStandardCPU):
                   dtype=self.model.dtype)
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, PYDTNN_EVENT_FINISHED)
 
+        dx:np.ndarray = self.temp_c_r_dx[:self.dx_shape_size].reshape(self.dx_shape, order="C")
+        dx.fill(0)  # NOTE: It is necessary that dx is filled with 0s.
+
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.COMP_DX_COL2IM)
         row2im_nhwc_cython(rows, dx,
                            dy.shape[0], self.hi, self.wi, self.ci,
@@ -203,10 +218,10 @@ class Conv2DI2CCPU(Conv2DStandardCPU):
 
     def _backward_i2c_nchw(self, dy: np.ndarray) -> np.ndarray:
         """Version of the backward function that uses im2col and matmul"""
-        cols:np.ndarray = np.asarray(self.temp_bw[:, :(dy.shape[0] * self.ho * self.wo)], dtype=self.model.dtype, order="C", copy=None)
+        #cols:np.ndarray = np.asarray(self.temp_bw[:, :(dy.shape[0] * self.ho * self.wo)], dtype=self.model.dtype, order="C", copy=None)
+        cols = self.get_cols(dy.shape[0])
 
-        dx:np.ndarray = self.dx[:dy.shape[0], :]
-        dx.fill(0)  # NOTE: It is necessary that dx is filled with 0s.
+        self.dw = self.dw.reshape(self._dw_shape)
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.BACKWARD_TRANSPOSE_DY)
         dy_rows: np.ndarray = format_transpose(dy, "NCHW", "CNHW").reshape((self.co, -1), copy=None)
@@ -238,6 +253,9 @@ class Conv2DI2CCPU(Conv2DStandardCPU):
         np.matmul(w_cols, dy_rows, out=cols,
                   dtype=self.model.dtype, order='C')
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, PYDTNN_EVENT_FINISHED)
+
+        dx:np.ndarray = self.temp_c_r_dx[:self.dx_shape_size].reshape(self.dx_shape, order="C")
+        dx.fill(0)  # NOTE: It is necessary that dx is filled with 0s.
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.COMP_DX_COL2IM)
         col2im_nchw_cython(cols, dx,
