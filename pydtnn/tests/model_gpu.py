5813921ef5d0568580ae1b35c58f76f3071cf806
Miguel Ángel Prosper <mprosper@uji.es>
2026-02-03 07:42:15 +0100

    fix gpu test

--- pydtnn/tests/model_gpu.py
+++ pydtnn/tests/model_gpu.py
@@ -35,6 +35,7 @@ class ModelGpuTestCase(ModelCommonTestCase):
         # GPU model
         params = Params()
         params.model_name = model_name  # type: ignore
+        params.backend = "gpu"
         params.enable_cudnn = True  # type: ignore
         params.enable_cudnn_auto_conv_alg = True  # type: ignore
         params.tensor_format = TensorFormat.NHWC.upper()
