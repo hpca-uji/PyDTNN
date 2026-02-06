009318cd2e57dcbdf30cc663dd46713207ab443b
Miguel Ángel Prosper <mprosper@uji.es>
2026-02-06 07:47:32 +0100

    fix tracer

--- pydtnn/backends/direct/layers/conv_2d.py
+++ pydtnn/backends/direct/layers/conv_2d.py
@@ -72,7 +72,7 @@ class Conv2DDirect(AbstractConv2DStandardNumpy):
     def _forward_cd(self, x: np.ndarray, n=0) -> np.ndarray:
         """Version of the forward function that uses the convDirect library"""
 
-        self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.FORWARD_CONVDirect)
+        self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.FORWARD_CONVDIRECT)
         y = self.cd[n].conv_direct(self.weights, x, self.out,
                                    vpadding=self.hpadding, hpadding=self.wpadding,
                                    vstride=self.hstride, hstride=self.wstride,
