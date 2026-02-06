bbc5563845c56e5985a9977f90b0cb651fea6be5
Miguel Ángel Prosper <mprosper@uji.es>
2026-02-06 09:05:11 +0100

    fix format

--- pydtnn/layer_base.py
+++ pydtnn/layer_base.py
@@ -90,7 +90,7 @@ class LayerBase[T: Array](PromoteToBackend):
             props["path"] = ",".join(map(str, paths))
 
         props["name"] = self.canonical_name
-        props["backend"] = self.name.removeprefix(self.canonical_name).lower()
+        props["backend"] = self.name.removeprefix(self.canonical_name)
 
         if self.nparams > 0:
             props["params"] = self.nparams
