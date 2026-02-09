Fixed some bugs in different files: - Added a Batch normalization Cython (the backwards work well here). - Refactored the logic in numpy's batch_normalization (NOTE: the backward's method doesn't work well). - Fixed errors related to arrays in conv_2d - Small refactor in the cross_entropy_loss

pydtnn/backends/numpy/layers/batch_normalization.py
@@ -40,6 +40,8 @@ class BatchNormalizationNumpy(BatchNormalization[np.ndarray], LayerNumpy):
             vars_shape = (self.model.batch_size, self.ci)
         shape_ = (self.ci,)
 
+        self._xn = np.zeros(shape=vars_shape, dtype=self.model.dtype)
+
         self.gamma = np.full(shape_, self.gamma_init_val, dtype=self.model.dtype)
         self.beta = np.full(shape_, self.beta_init_val, dtype=self.model.dtype)
         self.running_mean = np.asarray(self.moving_mean_initializer(shape_, self.model.dtype))
@@ -85,6 +87,37 @@ class BatchNormalizationNumpy(BatchNormalization[np.ndarray], LayerNumpy):
             self._var = self.model.memory.ndarray(self._var_shape, dtype=self.model.dtype)
             self.dy_xn = self.model.memory.ndarray(self.dy_xn_shape, dtype=self.model.dtype)
 
+    def _training_fwd(self, x:np.ndarray, _mean: np.ndarray, _var: np.ndarray, y: np.ndarray) -> None:
+        # y = ((x - mean(x)) / sqrt(var(x) + epsilon)) * gamma + beta
+        # NOTE: Check this. (xn is already the mean)
+        np.subtract(x, _mean, out=self.xn,
+                    dtype=self.model.dtype)
+
+        np.add(_var, self.epsilon, out=self.std,
+               dtype=self.model.dtype)
+        np.sqrt(self.std, out=self.std,
+                dtype=self.model.dtype)
+
+        np.divide(self.xn, self.std, out=self.xn,
+                  dtype=self.model.dtype)
+        np.multiply(self.gamma, self.xn, out=y,
+                    dtype=self.model.dtype)
+        np.add(y, self.beta, out=y,
+               dtype=self.model.dtype)
+    # ---
+
+    def _training_bwd(self, dx: np.ndarray, dy: np.ndarray) -> None:
+        n = dy.shape[0]
+        # dx = (self.gamma / (self.std * n)) * (n * dy - self.xn * self.dgamma - self.dbeta)
+        np.multiply(self.std, n, out=dx)
+        np.divide(self.gamma, dx, out=dx)
+        np.multiply(n, dy, out=dy)
+        np.multiply(self.xn, self.dgamma, out=self.xn)
+        np.subtract(dy, self.xn, out=dy)
+        np.subtract(dy, self.dbeta, out=dy)
+        np.multiply(dx, dy, out=dx)
+    # ---
+
     def forward(self, x: np.ndarray) -> np.ndarray:
 
         self.y_dx: np.ndarray
@@ -97,16 +130,16 @@ class BatchNormalizationNumpy(BatchNormalization[np.ndarray], LayerNumpy):
         # else: x = x (no reshape needed)
 
         y: np.ndarray = self.y_dx[:x.shape[0], :]
-        self.xn = x
+        self.xn: np.ndarray = self._xn[:x.shape[0], :]
 
         if self.model.mode is Model.Mode.EVALUATE:
             _mean = self.running_mean
             _var = self.running_var
         else:  # Model.Mode.TRAIN:
-            _mean = self._mean
-            _var = self._var
-            np.mean(self.xn, axis=0, dtype=self.model.dtype, out=_mean)
-            np.var(self.xn, axis=0, dtype=self.model.dtype, out=_var)
+            _mean: np.ndarray = self._mean
+            _var: np.ndarray = self._var
+            np.mean(x, axis=0, dtype=self.model.dtype, out=_mean)
+            np.var(x, axis=0, dtype=self.model.dtype, out=_var)
 
             inv_momentum = (1.0 - self.momentum)
             # self.running_mean = self.momentum * self.running_mean + inv_momentum * _mean
@@ -127,22 +160,7 @@ class BatchNormalizationNumpy(BatchNormalization[np.ndarray], LayerNumpy):
         # anyways:
 
         # bn_training_fwd_cython(x, y, self.xn, self.std, self.gamma, self.beta, _mean, _var, self.epsilon)
-        # y = ((x - mean(x)) / sqrt(var(x) + epsilon)) * gamma + beta
-        np.subtract(self.xn, _mean, out=self.xn,
-                    dtype=self.model.dtype)
-
-        np.add(_var, self.epsilon, out=self.std,
-               dtype=self.model.dtype)
-        np.sqrt(self.std, out=self.std,
-                dtype=self.model.dtype)
-
-        np.divide(self.xn, self.std, out=self.xn,
-                  dtype=self.model.dtype)
-        np.multiply(self.gamma, self.xn, out=y,
-                    dtype=self.model.dtype)
-        np.add(y, self.beta, out=y,
-               dtype=self.model.dtype)
-
+        self._training_fwd(x, _mean, _var, y)
         if self.spatial:
             y = y.reshape((n, self.hi, self.wi, self.ci))
             y = format_transpose(y, TensorFormat.NHWC, self.model.tensor_format)
@@ -162,21 +180,14 @@ class BatchNormalizationNumpy(BatchNormalization[np.ndarray], LayerNumpy):
             num_elems = n
 
         dx: np.ndarray = self.y_dx[: num_elems, :]
+        #dx.fill(0)
         dy_xn: np.ndarray = self.dy_xn[: num_elems, :]
-        dy_xn.fill(0)
 
         np.multiply(dy, self.xn, out=dy_xn, dtype=self.model.dtype)
         np.sum(dy_xn, axis=0, out=self.dgamma, dtype=self.model.dtype)
         np.sum(dy, axis=0, out=self.dbeta, dtype=self.model.dtype)
 
-        # dx = (self.gamma / (self.std * n)) * (n * dy - self.xn * self.dgamma - self.dbeta)
-        np.multiply(self.std, n, out=dx)
-        np.divide(self.gamma, dx, out=dx)
-        np.multiply(n, dy, out=dy)
-        np.multiply(self.xn, self.dgamma, out=self.xn)
-        np.subtract(dy, self.xn, out=dy)
-        np.subtract(dy, self.dbeta, out=dy)
-        np.multiply(dx, dy, out=dx)
+        self._training_bwd(dx, dy)
 
         if self.spatial:
             dx = dx.reshape((n, self.hi, self.wi, self.ci), copy=False)

pydtnn/backends/numpy/layers/conv_2d.py
@@ -38,9 +38,6 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
                 raise NotImplementedError(f"\"{self.model.tensor_format}\" format not implemented.")
         # -
 
-        self.temp_bw_shape = self._x_cr_shape
-        self.temp_bw_shape_size = np.prod(self.temp_bw_shape)
-
         y_shape = (dim_n, self.co)
         self.y_size = np.prod(y_shape)
 
@@ -55,13 +52,13 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
 
         self.dx_shape_size = int(np.prod(self.dx_shape))
         # NOTE: These attributes only store data, their values before the operation doesn't matter; they're initalized due avoid warnings in "LayerAndActivationBase.export".
-        self.temp_c_r_dx = np.zeros(shape=(max(np.prod(self._x_cr_shape), self.dx_shape_size), ), dtype=self.model.dtype)
-        # self.temp_c_r_dx: Temporal array where the cols/rows and dx values are stored.
-        self.memory_used += self.temp_c_r_dx.nbytes
+        self.temp_c_r = np.zeros(shape=self._x_cr_shape, dtype=self.model.dtype)
+        # self.temp_c_r_dx: Temporal array where the forward and batckward's cols/rows are stored
+        self.memory_used += self.temp_c_r.nbytes
 
-        self.temp_y_bc_br = np.zeros(shape=(max(self.y_size, self.temp_bw_shape_size), ), dtype=self.model.dtype)
+        self.temp_y_dx = np.zeros(shape=(max(self.y_size, self.dx_shape_size), ), dtype=self.model.dtype)
         # self.temp_y_bc_br: Temporal array where the y and backward's cols/rows values are stored.
-        self.memory_used += self.temp_y_bc_br.nbytes
+        self.memory_used += self.temp_y_dx.nbytes
 
         self.memory_used += self.tmp_memory_used
     # ---
@@ -69,23 +66,29 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
     def get_rows(self, batch_size: int) -> np.ndarray:
         dim_n = batch_size * self.ho * self.wo
         shape = (dim_n, self.dim_c)
-        x_rows: np.ndarray = self.temp_c_r_dx[:np.prod(shape)]
+        x_rows: np.ndarray = self.temp_c_r[:np.prod(shape)]
         x_rows = x_rows.reshape(shape)
         return x_rows
 
     def get_cols(self, batch_size: int) -> np.ndarray:
         dim_n = batch_size * self.ho * self.wo
         shape = (self.dim_c, dim_n)
-        x_cols: np.ndarray = self.temp_c_r_dx[:np.prod(shape)]
+        x_cols: np.ndarray = self.temp_c_r[:np.prod(shape)]
         x_cols = x_cols.reshape(shape)
         return x_cols
 
     def get_y(self, batch_size: int) -> np.ndarray:
         dim_n = batch_size * self.ho * self.wo
         shape = (dim_n, self.co)
-        y: np.ndarray = self.temp_y_bc_br[:np.prod(shape)]
+        y: np.ndarray = self.temp_y_dx[:np.prod(shape)]
         y = y.reshape(shape)
         return y
+
+    def get_dx(self, batch_size: int) -> np.ndarray:
+        shape = self.model.encode_shape((batch_size, self.ci, self.hi, self.wi))
+        dx: np.ndarray = self.temp_y_dx[:np.prod(shape)]
+        dx = dx.reshape(shape)
+        return dx
 
 ##########################################################################################################################
 ##########################################################################################################################
@@ -259,7 +262,6 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
         """Version of the backward function that uses im2col and matmul"""
 
         # res = np.asarray(self.res_bw[:(dy.shape[0] * self.ho * self.wo), :], dtype=self.model.dtype)
-        rows: np.ndarray = self.get_rows(dy.shape[0])
         self.dw = self.dw.reshape(self._dw_shape)
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.BACKWARD_TRANSPOSE_DY)
@@ -275,6 +277,8 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.BACKWARD_RESHAPE_DW)
         self.dw = self.dw.reshape(self.weights.shape, copy=False)
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, PYDTNN_EVENT_FINISHED)
+
+        rows: np.ndarray = self.get_rows(dy.shape[0]) # NOTE: rows shares the memory with self.x_rows
 
         # Biases gradient
         if self.use_bias:
@@ -293,7 +297,7 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
                   dtype=self.model.dtype)
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, PYDTNN_EVENT_FINISHED)
 
-        dx: np.ndarray = self.temp_c_r_dx[:self.dx_shape_size].reshape(self.dx_shape)[:dy.shape[0]]
+        dx: np.ndarray = self.get_dx(dy.shape[0])
         dx.fill(0)  # NOTE: It is necessary that dx is filled with 0s.
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.COMP_DX_COL2IM)
@@ -305,8 +309,6 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
     def _backward_i2c_nchw(self, dy: np.ndarray) -> np.ndarray:
         """Version of the backward function that uses im2col and matmul"""
         # cols:np.ndarray = np.asarray(self.temp_bw[:, :(dy.shape[0] * self.ho * self.wo)], dtype=self.model.dtype)
-        cols = self.get_cols(dy.shape[0])
-
         self.dw = self.dw.reshape(self._dw_shape)
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.BACKWARD_TRANSPOSE_DY)
@@ -318,6 +320,8 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
         np.matmul(dy_rows, self.x_cols.T, out=self.dw,
                   dtype=self.model.dtype)
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, PYDTNN_EVENT_FINISHED)
+
+        cols = self.get_cols(dy.shape[0])  # NOTE: cols shares the memory with self.x_cols
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.BACKWARD_RESHAPE_DW)
         self.dw = self.dw.reshape(self.weights.shape)
@@ -340,7 +344,7 @@ class Conv2DNumpy(AbstractConv2DStandardNumpy):
                   dtype=self.model.dtype, order='C')
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, PYDTNN_EVENT_FINISHED)
 
-        dx: np.ndarray = self.temp_c_r_dx[:self.dx_shape_size].reshape(self.dx_shape)[:dy.shape[0]]
+        dx: np.ndarray = self.get_dx(dy.shape[0])
         dx.fill(0)  # NOTE: It is necessary that dx is filled with 0s.
 
         self.model.tracer.emit_event(PYDTNN_OPS_EVENT, self.id * PYDTNN_OPS_EVENTS + PYDTNN_OPS_EVENT_enum.COMP_DX_COL2IM)

pydtnn/backends/numpy/losses/categorical_cross_entropy.py
@@ -37,23 +37,22 @@ class CategoricalCrossEntropyNumpy(CategoricalCrossEntropy[np.ndarray], LossNump
         _y_pred: np.ndarray = self._y_pred[:b]
         _y_pred_op: np.ndarray = self._y_pred_op[:b]
         dx: np.ndarray = self.dx[:b]
+        dx.fill(0)
 
         # Common
         b_range: np.ndarray = np.arange(b)
         np.clip(y_pred, a_min=self.eps, a_max=(1 - self.eps), out=_y_pred)
         np.argmax(y_targ, axis=1, out=_argmax)
-        _y_pred_sliced = _y_pred[b_range, _argmax]
 
         # Loss
-        np.log(_y_pred_sliced, out=_y_pred_op)
+        np.log(_y_pred[b_range, _argmax], out=_y_pred_op)
         loss: float = -np.sum(_y_pred_op) / b
 
         # DX
         # dx: np.ndarray = np.copy(y_targ)
         # dx_amax: np.ndarray = np.argmax(dx, axis=1)
         # dx[b_range, dx_amax] /= (-_y_pred_sliced[b_range, dx_amax] * batch_size)
-        dx.fill(0)
-        np.multiply(-1 * batch_size, _y_pred_sliced, out=_y_pred_sliced)
-        dx[b_range, _argmax] = y_targ[b_range, _argmax] / _y_pred_sliced
-
+        dx[:] = y_targ
+        np.multiply(-1 * batch_size, _y_pred, out=_y_pred)
+        dx[b_range, _argmax] /= _y_pred[b_range, _argmax]
         return loss, np.asarray(dx, dtype=self.model.dtype)