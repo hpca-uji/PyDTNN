c03b78bf79c32050f069d7bf11b3ab0d43276e54
Miguel Ángel Prosper <mprosper@uji.es>
2026-02-16 09:21:59 +0100

    update export script to match

--- scripts/export_dataset.py
+++ scripts/export_dataset.py
@@ -8,7 +8,7 @@ from pydtnn.model import Model
 from pydtnn.parser import PydtnnArgumentParser
 
 parser = PydtnnArgumentParser()
-parser.add_argument("--export-split-weights", type=str, default="1")
+parser.add_argument("--export-split-weights", type=str, default="")
 model = Model(**parser.to_dict())
-split_weights = list(map(float, model.export_split_weights.split(",")))
+split_weights = list(filter(None, map(float, model.export_split_weights.split(","))))
 model.dataset.export_archive(split_weights=split_weights)
