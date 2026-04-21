6f790e207290fb79cfd741bc9255883bb7b94379
Miguel Ángel Prosper <mprosper@uji.es>
2026-04-21 09:12:50 +0200

    cython shared utility module

--- setup.py
+++ setup.py
@@ -6,14 +6,23 @@ import numpy
 from Cython.Build import cythonize
 
 
+ext_modules = [
+    Extension("pydtnn.utils._cyutility", sources=["pydtnn/utils/_cyutility.c"])
+]
+
+for pyx in Path("pydtnn").rglob("*.pyx"):
+    ext_modules.append(Extension(
+        ".".join(pyx.with_suffix("").parts),
+        [str(pyx)],
+        extra_compile_args=["-fopenmp", "-O3", "-march=native", "-g0"],
+        extra_link_args=["-fopenmp"],
+        include_dirs=[numpy.get_include()],
+    ))
+
 setup(
-    ext_modules=cythonize([
-        Extension(
-            ".".join(pyx.with_suffix("").parts),
-            [str(pyx)],
-            extra_compile_args=["-fopenmp", "-O3", "-march=native", "-g0"],
-            extra_link_args=["-fopenmp"],
-            include_dirs=[numpy.get_include()],
-        ) for pyx in Path("pydtnn").rglob("*.pyx")
-    ], language_level=3),
+    ext_modules=cythonize(
+        ext_modules,
+        language_level=3,
+        shared_utility_qualified_name="pydtnn.utils._cyutility"
+    ),
 )
