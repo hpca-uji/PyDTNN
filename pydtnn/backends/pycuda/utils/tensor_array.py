1a34ef3aed91a6951eedd1b10316b29c266753bd
Miguel Ángel Prosper <mprosper@uji.es>
2026-04-22 10:56:34 +0200

    add tensor_array cpu helpers

--- pydtnn/backends/pycuda/utils/tensor_array.py
+++ pydtnn/backends/pycuda/utils/tensor_array.py
@@ -108,6 +108,82 @@ class TensorArray:
         self._meta_init(gpu_arr, desc)
     # ---
 
+    def to_cpu(self) -> np.ndarray:
+        value = self.ary.get()
+
+        match len(self.ary.shape):
+            case 1:
+                match self.tensor_format:
+                    case TensorFormat.NCHW:
+                        # shape = (1, *ary.shape, 1, 1)
+                        value = np.squeeze(value, axis=(0, 2, 3))
+                    case TensorFormat.NHWC:
+                        # shape = (1, 1, 1, *ary.shape)
+                        value = np.squeeze(value, axis=(0, 1, 2))
+                    case tensor_format:
+                        raise NotImplementedError(f"Unsupported tensor format {tensor_format}!")
+            case 2:
+                match self.tensor_format:
+                    case TensorFormat.NCHW:
+                        # shape = (*ary.shape, 1, 1)
+                        value = np.squeeze(value, axis=(2, 3))
+                    case TensorFormat.NHWC:
+                        # shape = (ary.shape[0], 1, 1, ary.shape[1])
+                        value = np.squeeze(value, axis=(1, 2))
+                    case tensor_format:
+                        raise NotImplementedError(f"Unsupported tensor format {tensor_format}!")
+            case 3:
+                match self.tensor_format:
+                    case TensorFormat.NCHW:
+                        # shape = (ary.shape[0], 1, ary.shape[1], ary.shape[2])
+                        value = np.squeeze(value, axis=(1,))
+                    case TensorFormat.NHWC:
+                        raise NotImplementedError("Shape padding not implemented for 3-dim shape on NHWC")
+            case 4:
+                # shape = ary.shape
+                pass  # exact
+            case _:
+                raise ValueError(f"The expected len shape are 1, 2, 3 or 4. Shape received: {len(self.ary.shape)}.")
+
+        return value
+
+    def from_cpu(self, value: np.ndarray) -> None:
+        match len(value.shape):
+            case 1:
+                match self.tensor_format:
+                    case TensorFormat.NCHW:
+                        # shape = (1, *ary.shape, 1, 1)
+                        value = np.expand_dims(value, axis=(0, 2, 3))
+                    case TensorFormat.NHWC:
+                        # shape = (1, 1, 1, *ary.shape)
+                        value = np.expand_dims(value, axis=(0, 1, 2))
+                    case tensor_format:
+                        raise NotImplementedError(f"Unsupported tensor format {tensor_format}!")
+            case 2:
+                match self.tensor_format:
+                    case TensorFormat.NCHW:
+                        # shape = (*ary.shape, 1, 1)
+                        value = np.expand_dims(value, axis=(2, 3))
+                    case TensorFormat.NHWC:
+                        # shape = (ary.shape[0], 1, 1, ary.shape[1])
+                        value = np.expand_dims(value, axis=(1, 2))
+                    case tensor_format:
+                        raise NotImplementedError(f"Unsupported tensor format {tensor_format}!")
+            case 3:
+                match self.tensor_format:
+                    case TensorFormat.NCHW:
+                        # shape = (ary.shape[0], 1, ary.shape[1], ary.shape[2])
+                        value = np.expand_dims(value, axis=(1,))
+                    case TensorFormat.NHWC:
+                        raise NotImplementedError("Shape padding not implemented for 3-dim shape on NHWC")
+            case 4:
+                # shape = ary.shape
+                pass  # exact
+            case _:
+                raise ValueError(f"The expected len shape are 1, 2, 3 or 4. Shape received: {len(value.shape)}.")
+
+        self.ary.set(value)
+
     def copy(self):
         """ NumPy-like copy. """
         return copy.deepcopy(self)
