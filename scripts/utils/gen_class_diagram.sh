2450f9df0855f3cd0ea5ad57cec72cb7d21819fe
Miguel Ángel Prosper <mprosper@uji.es>
2026-04-27 16:01:39 +0200

    simple cls diagram

--- scripts/utils/gen_class_diagram.sh
+++ scripts/utils/gen_class_diagram.sh
@@ -13,7 +13,7 @@ sed -E 's|(\w+):(\w+),(.*)|\1:\2\n\1:\3|g' |  # Expand multiple inheritance (4)
 sed -E 's|(\w+):(\w+),(.*)|\1:\2\n\1:\3|g' |  # Expand multiple inheritance (5)
 sed -E 's|(\w+\.)+||g' |  # Remove package prefixes
 sort -u |  # Unique classes
-sed -E 's|^|    |; s|:| -> |' |  # DOT format
+sed 's|:| -> |' |  # DOT format
 sed '1idigraph {' |  # DOT header
 sed '2irankdir=RL' |  # DOT configuration
 sed '$a}' |  # DOT footer
