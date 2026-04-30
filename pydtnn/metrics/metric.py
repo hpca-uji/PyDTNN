4e929a4c036c8fc0bb899eefad44a4b10c5fcd18
Paul <pluijter@uji.es>
2026-04-30 10:07:10 +0200

    Fixed error in metrics

--- pydtnn/metrics/metric.py
+++ pydtnn/metrics/metric.py
@@ -19,9 +19,10 @@ class Metric[T: Array](Base):
         self.eps = eps
 
     def _model_init(self) -> None:
+        self.dtype: np.dtype = np.dtype(np.float32) if np.issubdtype(self.model.dtype, np.int32) else self.model.dtype
+        # NOTE: self.dtype is necessary before calling super.
         super()._model_init()
         self.shape = (self.model.batch_size, *self.model.output_shape)
-        self.dtype: np.dtype = np.dtype(np.float32) if np.issubdtype(self.model.dtype, np.int32) else self.model.dtype
 
     @abstractmethod
     def compute(self, y_pred: T, y_targ: T) -> float | np.ndarray:
