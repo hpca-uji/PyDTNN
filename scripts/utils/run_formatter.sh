fd531162636f24105561f4f6610458c1a87f65c6
Miguel Ángel Prosper <miguelangel.prosper@gmail.com>
2026-04-30 13:48:04 +0200

    better formatter

--- scripts/utils/run_formatter.sh
+++ scripts/utils/run_formatter.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Fixup sources for PEP8
 SRC="${1:-.}"
 MAX_LINE_LENGTH="${PEP8_MAX_LINE_LENGTH:-200}"
+PYS=('(' -name '*.py' -o -name '*.py' -o -name '*.pyi' -o -name '*.pyx' ')')
 
-find "${SRC:?}" -name '*.py' -exec absolufy-imports '{}' '+'
+find "${SRC:?}" "${PYS[@]}" -exec absolufy-imports '{}' '+'
+EXPR='#{2,}' && find "${SRC:?}" "${PYS[@]}" -exec grep -qEe "${EXPR:?}" '{}' ';' -exec sed -Ei "s/${EXPR:?}/#/g" '{}' ';'
+EXPR='\s+#+$' && find "${SRC:?}" "${PYS[@]}" -exec grep -qEe "${EXPR:?}" '{}' ';' -exec sed -Ei "s/${EXPR:?}//g" '{}' ';'
+EXPR='^\s*#\s*[_=/-]+\s*(end|END)\b' && find "${SRC:?}" "${PYS[@]}" -exec grep -qEe "${EXPR:?}" '{}' ';' -exec sed -Ei "/${EXPR:?}/d" '{}' ';'
+EXPR='^\s*#\s*[_=/-]{2,}[^A-Z]*$' && find "${SRC:?}" "${PYS[@]}" -exec grep -qEe "${EXPR:?}" '{}' ';' -exec sed -Ei "/${EXPR:?}/d" '{}' ';'
 autopep8 -iaaar --max-line-length "${MAX_LINE_LENGTH:?}" "${SRC:?}"
-isort "${SRC:?}"
+isort -q "${SRC:?}"
