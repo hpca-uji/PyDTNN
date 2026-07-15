dc6d3995e6a0ec3503279721f4163f60fa574527
Paul <pluijter@uji.es>
2026-07-15 09:29:23 +0200

    Refactor

---
+++ pydtnn/utils/pytorch.py
@@ -0,0 +1,26 @@
+"""Get a model from PyTorch converted to PyDTNN."""
+
+import torch
+
+from collections.abc import Sequence
+
+from pydtnn.utils.constants import ArrayShape
+
+from pydtnn.abstract.layerable import Layerable
+from pydtnn.converters.pytorch2pydtnn.model_convertor import get_layers_from_torch
+
+
+def from_pytorch(
+    input_shape: ArrayShape,
+    torch_model: torch.nn.Module,
+) -> Sequence[Layerable]:
+    """Returns a PyDTNN conversion from a PyTorch model.
+
+    Args:
+        input_shape (ArrayShape): Data's input shape.
+        output_shape (ArrayShape): The output's shape.
+        torch_model_func (Callable[[ArrayShape], torch.nn.Module]): A function that will provide a PyTorch model.
+    """
+    layers = get_layers_from_torch(model=torch_model, input_shape=input_shape)
+
+    return layers
