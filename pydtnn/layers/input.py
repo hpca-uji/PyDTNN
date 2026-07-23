9a236be9bbe6d7a0b53bc033771a2afe86f4d7cd
Miguel Ángel Prosper <mprosper@uji.es>
2026-07-23 09:08:19 +0200

    set input layer prev_shape

--- pydtnn/layers/input.py
+++ pydtnn/layers/input.py
@@ -5,7 +5,7 @@ import logging
 import numpy as np
 
 from pydtnn.layers.abstract.layer import Layer
-from pydtnn.utils.constants import Array
+from pydtnn.utils.constants import Array, ArrayShape
 
 __all__ = ("Input",)
 
@@ -23,6 +23,10 @@ class Input[T: Array](Layer[T]):  # noqa: D101 (generics not detected)
             shape (tuple): The shape of the input data.
         """
         super().__init__(shape)
+
+    def _model_init(self, prev_shape: ArrayShape, x: T | None) -> None:
+        """Initialize layer state within the model context."""
+        super()._model_init(prev_shape or self.shape, x)
 
     def _sync_x_y(self, x_batch: np.ndarray, y_batch: np.ndarray) -> tuple[T, T]:
         """
